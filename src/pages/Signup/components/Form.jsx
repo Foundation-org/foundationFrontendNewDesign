bed56a4ed78ee68d83bd60c1c9b263a6cfc174ef
Hamzazaheer1 <mhamzazaheer1@gmail.com>
2023-11-04 19:15:21 +0500

    Login page password strength visualizer added + some styles fixes

--- src/pages/Signup/components/Form.jsx
+++ src/pages/Signup/components/Form.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
 import Input from '../../../components/Input';
+import PasswordStrengthBar from 'react-password-strength-bar';
 
 const Form = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [reTypePassword, setReTypePassword] = useState('');
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onPassChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onReTypePassChange = (e) => {
+    setReTypePassword(e.target.value);
+  };
+
   return (
-    <form className="w-full flex flex-col gap-11 my-6">
+    <form className="w-full flex flex-col gap-11 my-6 dark:text-white text-gray-600">
       <Input
         type="email"
         id="email"
         label="Email Address"
-        className="border-b py-1 focus:outline-none focus:border-white focus:border-b-2 transition-colors peer bg-white dark:bg-dark w-full border-white"
+        className="border-b py-1 focus:outline-none dark:focus:border-white focus:border-[#C0C0C0] focus:border-b-2 transition-colors peer bg-white dark:bg-dark w-full border-[#C0C0C0] dark:border-white"
         autoComplete="off"
+        onChange={onEmailChange}
       />
       <div>
         <div className="relative">
@@ -16,8 +35,9 @@ const Form = () => {
             type="password"
             id="password"
             label="Password"
-            className="border-b py-1 focus:outline-none focus:border-white focus:border-b-2 transition-colors peer bg-white dark:bg-dark w-full border-white"
+            className="border-b py-1 focus:outline-none dark:focus:border-white focus:border-[#C0C0C0] focus:border-b-2 transition-colors peer bg-white dark:bg-dark w-full border-white"
             autoComplete="off"
+            onChange={onPassChange}
           />
           <img
             src="/assets/svgs/blind.svg"
@@ -25,15 +45,9 @@ const Form = () => {
             className="absolute right-2 -top-2"
           />
         </div>
-        <div className="w-full bg-gray-200 rounded-full h-[3px] flex gap-2">
-          <div className="bg-green h-[3px] w-[25%]"></div>
-          <div className="bg-green h-[3px] w-[25%]"></div>
-          <div className="bg-green h-[3px] w-[25%]"></div>
-          <div className="bg-gray-200 dark:bg-white h-[3px] w-[25%]"></div>
+        <div className="-mt-1">
+          <PasswordStrengthBar password={password} />
         </div>
-        <p className="text-end text-green-200 text-[12px] mt-2 leading-[18px] tracking-[0.048px]">
-          Good
-        </p>
       </div>
       <div>
         <div className="relative">
@@ -43,6 +57,7 @@ const Form = () => {
             label="Re-Type Password"
             className="border-b py-1 focus:outline-none focus:border-white focus:border-b-2 transition-colors peer bg-white dark:bg-dark w-full border-white"
             autoComplete="off"
+            onChange={onReTypePassChange}
           />
           <img
             src="/assets/svgs/blind.svg"
@@ -50,15 +65,9 @@ const Form = () => {
             className="absolute right-2 -top-2"
           />
         </div>
-        <div className="w-full bg-gray-200 rounded-full h-[3px] flex gap-2">
-          <div className="bg-green h-[3px] w-[25%]"></div>
-          <div className="bg-green h-[3px] w-[25%]"></div>
-          <div className="bg-green h-[3px] w-[25%]"></div>
-          <div className="bg-gray-200 dark:bg-white h-[3px] w-[25%]"></div>
+        <div className="-mt-1">
+          <PasswordStrengthBar password={reTypePassword} />
         </div>
-        <p className="text-end text-green-200 text-[12px] mt-2 leading-[18px] tracking-[0.048px]">
-          Good
-        </p>
       </div>
     </form>
   );
