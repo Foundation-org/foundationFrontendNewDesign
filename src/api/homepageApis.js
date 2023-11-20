2c659aa841a6cf11ff520d404bbbb8a66e0f6f9f
wamiq <wamiqakram@gmail.com>
2023-11-20 10:17:27 +0500

    integerated pagination and filters on feed

--- src/api/homepageApis.js
+++ src/api/homepageApis.js
@@ -1,35 +1,45 @@
 import api from './Axios';
 
+// For Search 
+export const searchQuestions = async (searchTerm) => {
+  if (searchTerm !== '') {
+    const response = await api.post(`/search/easySearch`, {
+      term: searchTerm,
+
+    });
+    return response.data; // Assuming the relevant data is in the response.data property
+  }
+};
+
+
+// ============ Filters
+
+// For Default 
 export const getAllQuestsWithDefaultStatus = async (params) => {
   return await api.post('/infoquestions/getAllQuestsWithDefaultStatus', params);
 };
 
-export const searchQuestions = async (searchTerm) => {
-  if (searchTerm !== '')
-    return await api.post(`/search/easySearch?term=${searchTerm}`);
-};
-
-// ============ Filters
 // For Unanswered
-export const getAllUnanswered = async (searchTerm) => {
-  if (searchTerm !== '')
-    return await api.post(`/infoquestions/getAllQuestsWithOpenInfoQuestStatus`);
+export const getAllUnanswered = async (params) => {
+    return await api.post(`/infoquestions/getAllQuestsWithOpenInfoQuestStatus`,params);
 };
 
 // For Answered
-export const getAllAnswered = async (searchTerm) => {
-  if (searchTerm !== '')
-    return await api.post(`/infoquestions/getAllQuestsWithAnsweredStatus`);
+export const getAllAnswered = async (params) => {
+    return await api.post(`/infoquestions/getAllQuestsWithAnsweredStatus`,params);
 };
 
 // For Correct
-export const getAllCorrect = async (searchTerm) => {
-  if (searchTerm !== '')
-    return await api.post(`/infoquestions/getAllQuestsWithCorrectStatus`);
+export const getAllCorrect = async (params) => {
+    return await api.post(`/infoquestions/getAllQuestsWithCorrectStatus`,params);
 };
 
 // For InCorrect
-export const getAllInCorrect = async (searchTerm) => {
-  if (searchTerm !== '')
-    return await api.post(`/infoquestions/getAllQuestsWithIncorrectStatus`);
+export const getAllInCorrect = async (params) => {
+    return await api.post(`/infoquestions/getAllQuestsWithIncorrectStatus`,params);
+};
+
+// For Changable
+export const getAllChangable = async (params) => {
+    return await api.post(`/infoquestions/getAllQuestsWithChangeAnsStatus`,params);
 };
--- src/pages/Dashboard/pages/Main.jsx
+++ src/pages/Dashboard/pages/Main.jsx
@@ -1,5 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { getAllQuestsWithDefaultStatus } from '../../../api/homepageApis';
+import { getAllAnswered } from '../../../api/homepageApis';
+import { getAllCorrect } from '../../../api/homepageApis';
+import { getAllInCorrect } from '../../../api/homepageApis';
+import { getAllUnanswered } from '../../../api/homepageApis';
+import { getAllChangable } from '../../../api/homepageApis';
 import { useEffect, useState } from 'react';
 import QuestionCard from '../../../components/QuestionCard';
 import SidebarLeft from '../components/SidebarLeft';
@@ -8,121 +13,123 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import { getFilters } from '../../../features/filters/filtersSlice';
 import { useSelector } from 'react-redux';
 import { searchQuestions } from '../../../api/homepageApis';
+import { useQuery } from '@tanstack/react-query';
 
 const Main = () => {
-  const page = 1;
+  const pageLimit=10;
+  const [page,setPage]=useState(1);
   const filterStates = useSelector(getFilters);
-  const [userId, setUserId] = useState('');
-  const [pageLimit, setPageLimit] = useState(40);
   const [sliceStart, setSliceStart] = useState(0);
   const [sliceEnd, setSliceEnd] = useState(pageLimit);
-  const [response, setResponse] = useState([]);
-  const [searchResult, setSearchResult] = useState();
+  const [allData, setAllData] = useState([]);
 
-  console.log(filterStates);
 
-  const { mutateAsync: getQuestdefStatus } = useMutation({
-    mutationFn: getAllQuestsWithDefaultStatus,
-  });
 
-  const handleQuestdefStatus = async (params) => {
-    try {
-      const resp = await getQuestdefStatus(params);
 
-      if (resp.status === 200) {
-        setSliceStart(sliceEnd);
-        setSliceEnd(sliceEnd + pageLimit);
-        setResponse(resp.data);
-        // setResponse((pre) => [...pre, ...resp.data]);
+
+  const { data: feedData } = useQuery({
+    queryFn: async() => {
+
+      let params = {
+        _page: page,
+        _limit: pageLimit,
+        start: sliceStart,
+        end: sliceEnd,
+        uuid: localStorage.getItem('uId')
+      };
+  
+      // filter by sort
+      if (filterStates.filterBySort !== '') {
+        params = { ...params,  sort: filterStates.filterBySort };
       }
-    } catch (e) {
-      toast.error(e.response.data);
+  
+      // filter by type
+      if (
+        filterStates.filterByType !== '' &&
+        filterStates.filterByType !== 'All'
+      ) {
+        params = {
+          ...params,
+          type: filterStates.filterByType.toLowerCase(),
+        };
+      }
+  
+      // filter by scope
+      if (filterStates.filterByScope === 'Me') {
+        params = { ...params, filter: true };
+      }
+      console.log(filterStates);
+      if(page===1){
+        setAllData([])
+      }
+
+      if (filterStates.search === '') {
+        // Handle different filterStatus cases
+        let resultData;
+        if (filterStates.filterByStatus === 'All' || filterStates.filterByStatus === '' ) {
+          resultData = await getAllQuestsWithDefaultStatus(params);
+        } else if (filterStates.filterByStatus === 'Unanswered') {
+          resultData = await getAllUnanswered(params);
+        } else if (filterStates.filterByStatus === 'Answered') {
+          resultData = await getAllAnswered(params);
+        } else if (filterStates.filterByStatus === 'Correct') {
+          resultData = await getAllCorrect(params);
+        } else if (filterStates.filterByStatus === 'Incorrect') {
+          resultData = await getAllInCorrect(params);
+        } else if (filterStates.filterByStatus === 'Changeable') {
+          resultData = await getAllChangable(params);
+        }
+
+        console.log("results",resultData?.data);
+        return resultData?.data || {}; // Return a default value if resultData is undefined
+      } else {
+        return await searchQuestions(filterStates.search);
+      }
+    },
+    queryKey: ['FeedData', filterStates,page],
+    staleTime: 300000, // 5 minutes
+ 
+  });
+  useEffect(() => {
+    if (feedData && feedData.data) {
+      setAllData((prevData) => [
+        ...prevData,
+        ...feedData.data || [],
+      ]);
     }
-  };
+  }, [feedData]);
+
 
   useEffect(() => {
-    let params = {
-      _page: page,
-      _limit: pageLimit,
-      start: sliceStart,
-      end: sliceEnd,
-    };
+    setPage(1); // 
 
-    // filter by sort
-    if (filterStates.filterBySort !== '') {
-      params = { ...params, filter: true, sort: filterStates.filterBySort };
-    }
-
-    // filter by type
-    if (
-      filterStates.filterByType !== '' &&
-      filterStates.filterByType !== 'All'
-    ) {
-      params = {
-        ...params,
-        filter: true,
-        type: filterStates.filterByType.toLowerCase(),
-      };
-    }
-
-    // filter by scope
-    if (filterStates.filterByScope === 'Me') {
-      params = { ...params, filter: true, uuid: localStorage.getItem('uId') };
-    }
-
-    handleQuestdefStatus(params);
-  }, [pageLimit, filterStates]);
+    
+  }, [filterStates]);
 
   const fetchMoreData = () => {
+
+    console.log("fetch more called");
     const nextPage = page + 1;
-
-    let params = {
-      uuid: localStorage.getItem('uId'),
-      _page: nextPage,
-      _limit: pageLimit,
-      start: sliceStart,
-      end: sliceEnd,
-    };
-
-    handleQuestdefStatus(params);
+    const newSliceStart = sliceEnd;
+    const newSliceEnd = sliceEnd + pageLimit;
+    setSliceStart(newSliceStart);
+    setSliceEnd(newSliceEnd);
+    setPage(nextPage);
   };
+  
 
-  // Get Data based on search
-  const getSearchResult = async () => {
-    try {
-      const resp = await searchQuestions(filterStates.search);
-      if (resp) {
-        setSearchResult(resp.data);
-      } else {
-        console.log('API response is undefined.');
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    getSearchResult();
-  }, [filterStates.search]);
-
-  // Empty check
-  useEffect(() => {
-    if (filterStates.search === '') {
-      setSearchResult('');
-    }
-  }, [filterStates]);
 
   return (
     <>
       <SidebarLeft />
       <div className="bg-[#FCFCFD] dark:bg-[#06070a] shadow-inner-md w-full py-[27px] pl-6 pr-[23px] flex flex-col gap-[27px] h-[calc(100vh-96px)] overflow-y-auto no-scrollbar shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-        {searchResult ? (
+           {console.log("dataaaaaa"+allData)}
           <InfiniteScroll
-            dataLength={searchResult.length}
+            dataLength={allData?.length || 0}
             next={fetchMoreData}
-            hasMore={true}
+            hasMore={feedData?.hasNextPage}
             loader={
-              searchResult?.length === 0 ? (
+              allData?.length === 0 ? (
                 <h4>No data found...</h4>
               ) : (
                 <h4>Loading...</h4>
@@ -130,14 +137,14 @@ const Main = () => {
             }
             className="flex flex-col gap-[27px]"
           >
-            {searchResult?.map((item, index) => (
+            {allData?.map((item, index) => (
               <div key={index + 1}>
                 <QuestionCard
                   id={item._id}
                   img="/assets/svgs/dashboard/badge.svg"
                   alt="badge"
                   badgeCount="5"
-                  title="Ranked Choice"
+                  title={item?.whichTypeQuestion}
                   question={item?.Question}
                   btnColor={'bg-[#BB9D02]'}
                   btnText={'Change'}
@@ -146,37 +153,7 @@ const Main = () => {
               </div>
             ))}
           </InfiniteScroll>
-        ) : (
-          <InfiniteScroll
-            dataLength={response.length}
-            next={fetchMoreData}
-            hasMore={true}
-            loader={
-              response.length === 0 ? (
-                <h4>No data found...</h4>
-              ) : (
-                <h4>Loading...</h4>
-              )
-            }
-            className="flex flex-col gap-[27px]"
-          >
-            {response?.map((item, index) => (
-              <div key={index + 1}>
-                <QuestionCard
-                  id={item._id}
-                  img="/assets/svgs/dashboard/badge.svg"
-                  alt="badge"
-                  badgeCount="5"
-                  title="Ranked Choice"
-                  question={item?.Question}
-                  btnColor={'bg-[#BB9D02]'}
-                  btnText={'Change'}
-                  isBookmarked={false}
-                />
-              </div>
-            ))}
-          </InfiniteScroll>
-        )}
+      
       </div>
       <SidebarRight />
     </>
