7958e13f49a43607e784ceb429fdf264488a9b0b
Zubair <m.zobaire@gmail.com>
2023-12-09 23:31:11 +0500

    tooltip close icon color change, submit button fix, question updated

--- src/pages/Dashboard/pages/Quest/pages/MultipleChoice.jsx
+++ src/pages/Dashboard/pages/Quest/pages/MultipleChoice.jsx
@@ -299,6 +299,7 @@ const MultipleChoice = () => {
               e.target.value.trim() !== "" &&
               questionVerification(e.target.value.trim())
             }
+            value={question}
           />
           {/* <div className="relative"> */}
           <button
@@ -425,6 +426,7 @@ const MultipleChoice = () => {
         </div> */}
         <div className="flex w-full justify-end">
           <button
+            disabled={checkQuestionStatus?.isVerifiedQuestion && typedValues.every(item => item.optionStatus.isVerifiedAnswer === true) ? false : true} 
             className="mr-[28px] mt-[30px] w-fit rounded-[7.28px] bg-gradient-to-tr from-[#6BA5CF] to-[#389CE3] px-[24.5px] py-[3.8px] text-[10px] font-semibold leading-normal text-white tablet:mr-[70px] tablet:mt-[60px] tablet:rounded-[15.2px] tablet:px-[15.26px] tablet:py-[8.14px] tablet:text-[20.73px] xl:rounded-[23.6px] xl:px-[60px] xl:py-3 xl:text-[31.5px]"
             onClick={() => handleSubmit()}
           >
