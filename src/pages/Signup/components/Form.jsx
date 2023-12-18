1b803db78e5a1035f6d88959c63ae7081c8a6d0a
Abdullah Yaqoob <abdullahyaqoob38@gmail.com>
2023-12-18 14:42:02 +0500

    ico reversed, sign up icons fixed

--- src/pages/Signup/components/Form.jsx
+++ src/pages/Signup/components/Form.jsx
@@ -21,13 +21,14 @@ const Form = ({
 
   return (
     <form className="mt-6 flex w-full flex-col gap-11 text-gray-600 dark:text-white 5xl:gap-14 short:gap-[38px]">
-      <div className="relative">
+
+      <div className="relative w-full grid grid-cols-[1fr] items-center">
         <Input
           type="email"
           id="email"
           label="Email Address"
-          className="peer w-full border-b-[1px] border-[#C0C0C0] bg-white py-1 pl-[10px] text-[12px] transition-colors focus:border-b-2 focus:border-[#C0C0C0] focus:outline-none dark:border-white dark:bg-dark dark:focus:border-white md:text-[22.9px] short:py-0 taller:text-[16px]"
-          autoComplete="off"
+          className="peer w-full rounded-[2px] border-b-[1px] border-[#C0C0C0] bg-white py-1 pl-[10px] pr-8 text-[12px] transition-colors focus:border-b-2 focus:border-[#C0C0C0] focus:outline-none dark:border-white dark:bg-dark dark:focus:border-white md:text-[22.9px] short:py-0 taller:text-[16px]"
+          autoComplete="sign-email"
           onChange={onEmailChange}
           value={email}
         />
@@ -36,14 +37,14 @@ const Form = ({
             <img
               src="/assets/svgs/cancelDark.svg"
               alt="blind"
-              className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:-top-2"
+              className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
               onClick={handleCancel}
             />
           ) : (
             <img
               src="/assets/svgs/cancelLight.svg"
               alt="blind"
-              className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:top-[5px] taller:-top-[2px]"
+              className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
               onClick={handleCancel}
             />
           )
@@ -51,29 +52,30 @@ const Form = ({
       </div>
       <div className="flex flex-col gap-5">
         <div>
-          <div className="relative">
+
+          <div className="relative w-full grid grid-cols-[1fr] items-center">
+
             <Input
               type={inputType}
               id="password"
               label="Password"
-              className="peer w-full border-b-[1px] border-[#C0C0C0] bg-white py-1  text-[12px] transition-colors focus:border-b-2 focus:border-[#C0C0C0] focus:outline-none dark:border-white dark:bg-dark dark:focus:border-white md:text-[22.9px] short:py-0 taller:text-[16px]"
-              autoComplete="off"
+              className="peer w-full rounded-[2px] border-b-[1px] border-[#C0C0C0] bg-white py-1 pl-[10px] pr-8 text-[12px] transition-colors focus:border-b-2 focus:border-[#C0C0C0] focus:outline-none dark:border-white dark:bg-dark dark:focus:border-white md:text-[22.9px] short:py-0 taller:text-[16px]"
+              autoComplete="new-password"
               onChange={onPassChange}
             />
-
             {!showPassword ? (
               persistedTheme === "dark" ? (
                 <img
                   src="/assets/svgs/blind.svg"
                   alt="blind"
-                  className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:-top-2"
+                  className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
                   onClick={togglePasswordVisibility}
                 />
               ) : (
                 <img
                   src="/assets/svgs/eye-white.svg"
                   alt="blind"
-                  className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:top-[5px] taller:-top-[2px]"
+                  className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
                   onClick={togglePasswordVisibility}
                 />
               )
@@ -81,14 +83,14 @@ const Form = ({
               <img
                 src="/assets/svgs/eye.svg"
                 alt="blind"
-                className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:-top-2"
+                className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
                 onClick={togglePasswordVisibility}
               />
             ) : (
               <img
                 src="/assets/svgs/eyeLight.svg"
                 alt="blind"
-                className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:top-[5px] taller:-top-[2px]"
+                className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
                 onClick={togglePasswordVisibility}
               />
             )}
@@ -98,28 +100,31 @@ const Form = ({
           </div>
         </div>
         <div>
-          <div className="relative ">
+
+
+          <div className="relative w-full grid grid-cols-[1fr] items-center">
+
             <Input
               type={cnfmPassInputType}
-              id="retype-password"
-              label="Re-Type Password"
-              className="peer w-full border-b-[1px] border-[#C0C0C0] bg-white py-1  text-[12px] transition-colors focus:border-b-2 focus:border-[#C0C0C0] focus:outline-none dark:border-white dark:bg-dark dark:focus:border-white md:text-[22.9px] short:py-0 taller:text-[16px]"
-              autoComplete="off"
+              id="password"
+              label="Password"
+              className="peer w-full rounded-[2px] border-b-[1px] border-[#C0C0C0] bg-white py-1 pl-[10px] pr-8 text-[12px] transition-colors focus:border-b-2 focus:border-[#C0C0C0] focus:outline-none dark:border-white dark:bg-dark dark:focus:border-white md:text-[22.9px] short:py-0 taller:text-[16px]"
+              autoComplete="new-password"
               onChange={onReTypePassChange}
             />
-            {!showCnfmPassword ? (
+            {!showPassword ? (
               persistedTheme === "dark" ? (
                 <img
                   src="/assets/svgs/blind.svg"
                   alt="blind"
-                  className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:-top-2"
+                  className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
                   onClick={toggleCnfmPasswordVisibility}
                 />
               ) : (
                 <img
                   src="/assets/svgs/eye-white.svg"
                   alt="blind"
-                  className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:top-[5px] taller:-top-[2px]"
+                  className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
                   onClick={toggleCnfmPasswordVisibility}
                 />
               )
@@ -127,18 +132,21 @@ const Form = ({
               <img
                 src="/assets/svgs/eye.svg"
                 alt="blind"
-                className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:-top-2"
+                className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
                 onClick={toggleCnfmPasswordVisibility}
               />
             ) : (
               <img
                 src="/assets/svgs/eyeLight.svg"
                 alt="blind"
-                className="absolute right-2 top-[10px] h-[17px] w-[17px] cursor-pointer md:h-[30px] md:w-[30px] tablet:top-[5px] taller:-top-[2px]"
+                className="cursor-pointer absolute right-2 h-[17px] w-[17px] md:h-[30px] md:w-[30px] "
                 onClick={toggleCnfmPasswordVisibility}
               />
             )}
           </div>
+
+
+
           <div className="relative -top-1 mt-1 h-[19px]">
             {reTypePassword && (
               <PasswordStrengthBar password={reTypePassword} />
