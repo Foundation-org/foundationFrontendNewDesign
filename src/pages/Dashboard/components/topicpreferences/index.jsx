ed9c3b4600dfa4d722208e2e59df717dcd580839
Hamza Zaheer <malikhamza1619619@gmail.com>
2023-12-28 12:44:15 +0500

    revert recpatcha and test

--- src/pages/Dashboard/components/topicpreferences/index.jsx
+++ src/pages/Dashboard/components/topicpreferences/index.jsx
@@ -109,7 +109,7 @@ const TopicPreferences = ({ topicSearch, setTopicSearch }) => {
 
   return (
     <div className="w-full py-[2.94rem]">
-      <div className="mx-auto flex max-w-[80%] gap-7">
+      <div className="mx-auto flex max-w-[80%] items-center gap-7">
         <h1 className="text-[2.18rem] font-medium leading-normal text-[#535353]">
           Topic
         </h1>
@@ -117,25 +117,25 @@ const TopicPreferences = ({ topicSearch, setTopicSearch }) => {
           <input
             type="text"
             placeholder="Search here...."
-            className="h-[25px] w-full min-w-[215px] rounded-[8px] border-[1px] border-white bg-[#F6F6F6] px-3 text-[8.4px] text-gray-400 focus:outline-none dark:border-[#989898] dark:bg-[#000] dark:text-[#E8E8E8] tablet:h-[50.7px] tablet:text-[17.13px]"
+            className="h-[25px] w-full min-w-[215px] rounded-[8px] border-[1px] border-white bg-[#F3F3F3] px-3 text-[8.4px] text-gray-400 focus:outline-none dark:border-[#989898] dark:bg-[#000] dark:text-[#E8E8E8] tablet:h-[80px] tablet:rounded-[26px] tablet:pl-[45px] tablet:pr-[60px] tablet:text-[26px]"
             value={topicSearch}
             onChange={handleSearch}
           />
           {topicSearch && (
             <button
-              className="absolute right-3 top-[9px]"
+              className="absolute right-3 top-[9px] tablet:top-1/2 tablet:-translate-x-1/2 tablet:-translate-y-1/2"
               onClick={() => {
                 setTopicSearch("");
               }}
             >
-              <GrClose className="h-3 w-3 text-black dark:text-white" />
+              <GrClose className="h-3 w-3 text-[#C9C8C8] dark:text-white tablet:h-[27px] tablet:w-[27px]" />
             </button>
           )}
           {!topicSearch && (
             <img
               src="/assets/svgs/dashboard/search.svg"
               alt="search"
-              className="absolute right-[12px] top-[9px] h-3 w-3 tablet:top-3 tablet:h-[26.4px] tablet:w-[24.3px]"
+              className="absolute right-[12px] top-[9px] h-3 w-3 tablet:top-1/2 tablet:h-[26.4px] tablet:w-[24.3px] tablet:-translate-x-1/2 tablet:-translate-y-1/2"
             />
           )}
         </div>
@@ -143,11 +143,11 @@ const TopicPreferences = ({ topicSearch, setTopicSearch }) => {
       {/* columns */}
       <div className="mt-[2.88rem]">
         <DragDropContext onDragEnd={onDragEnd}>
-          <div className="flex justify-center gap-[1.44rem] ">
+          <div className="flex justify-center gap-[1.44rem]">
             {Object.values(columns).map((col) => (
               <Droppable droppableId={col.id}>
                 {(provided) => (
-                  <div className="flex w-[19.125rem] flex-col py-4">
+                  <div className="flex flex-col">
                     <h2 className="flex h-[4.18rem] w-full items-center justify-center rounded-t-[20px] bg-[#F2F2F2] text-center text-[28px] font-semibold text-[#535353]">
                       {col.id}
                     </h2>
