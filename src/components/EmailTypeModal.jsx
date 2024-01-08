2d9187c42c71b1957c8bec6e53124cf78f66c97f
Muhammad Zubair <93026937+izubaire@users.noreply.github.com>
2024-01-08 19:42:13 +0500

    Popup for email type

---
+++ src/components/EmailTypeModal.jsx
@@ -0,0 +1,107 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { useEffect } from 'react'
+import { Fragment, useState } from 'react'
+
+export default function EmailTypeModal({modalShow, email, handleEmailType}) {
+  let [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    setIsOpen(modalShow)
+  }, [modalShow])
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  }
+
+  return (
+    <>
+      {/* <div className="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          onClick={openModal}
+          className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+        >
+          Open dialog
+        </button>
+      </div> */}
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => 0}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg text-center font-medium leading-6 text-[#838383]"
+                  >
+                    {/* abc@work.com */}
+                    {email}
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-lg text-[#838383] text-center font-medium">
+                      Please select if this email is personal or professional.
+                    </p>
+                  </div>
+
+                  <div className='flex justify-center tablet:gap-8 gap-5 mt-4'>
+                    <label htmlFor="personal" className={`cursor-pointer flex justify-center items-center gap-3 py-2 bg-[#4A8DBD] rounded-lg tablet:min-w-[10rem] min-w-[8rem] ${value === "personal" ? 'border-dark-blue' : 'border-[#4A8DBD]'} border-[3px]`}>
+                        <div><img className='tablet:w-10 w-7 h-7 tablet:h-10' src="/assets/svgs/personal.svg" alt="personal" /></div>
+                        <p className='text-white font-medium text-lg'>Personal</p>
+                    </label>
+                    <input type="radio" hidden id='personal' name='radio' value='personal' onChange={(e) => handleChange(e)} />
+                    <label htmlFor="work" className={`cursor-pointer flex justify-center items-center gap-3 py-2 bg-[#4A8DBD] rounded-lg tablet:min-w-[10rem] min-w-[8rem] ${value === "work" ? 'border-dark-blue' : 'border-[#4A8DBD]'} border-[3px]`}>
+                        <div><img className='tablet:w-10 w-7 h-7 tablet:h-10' src="/assets/svgs/work.svg" alt="work" /></div>
+                        <p className='text-white font-medium text-lg'>Work</p>
+                    </label>
+                    <input type="radio" hidden id='work' name='radio' value='work' onChange={(e) => handleChange(e)} />
+                  </div>
+
+                  <div className="mt-4 text-center">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center items-center bg-blue-200 dark:bg-[#2759A5] text-center py-2 px-4 my-4 rounded-lg text-white font-medium text-lg"
+                      // onClick={closeModal}
+                      onClick={() => handleEmailType(value)}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
--- src/pages/Dashboard/components/SidebarRight.jsx
+++ src/pages/Dashboard/components/SidebarRight.jsx
@@ -8,14 +8,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../../../features/auth/authSlice";
 import Anchor from "../../../components/Anchor";
 import api from "../../../api/Axios";
+import Cookies from "js-cookie";
+import EmailTypeModal from "../../../components/EmailTypeModal";
 
 const SidebarRight = () => {
   const dispath = useDispatch();
   const navigate = useNavigate();
   const [response, setResponse] = useState();
   const [treasuryAmount, setTreasuryAmount] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
   const persistedTheme = useSelector((state) => state.utils.theme);
   const persistedUserInfo = useSelector((state) => state.auth.user);
+
 
   const sidebarList = [
     {
@@ -110,6 +114,9 @@ const SidebarRight = () => {
 
       if (resp.status === 200) {
         dispath(addUser(resp.data));
+        if(resp.data.requiredAction) {
+          setModalVisible(true);
+        }
       }
 
       // console.log(resp);
@@ -132,7 +139,7 @@ const SidebarRight = () => {
   };
 
   useEffect(() => {
-    handleUserInfo(localStorage.getItem("uId"));
+    handleUserInfo(Cookies.get("uId"));
     getTreasuryAmount();
   }, []);
 
@@ -149,97 +156,125 @@ const SidebarRight = () => {
   });
 
   useEffect(() => {
-    const uId = localStorage.getItem("uId");
+    const uId = Cookies.get("uId");
 
     if (!uId) {
       createGuest();
     }
   }, []);
 
+  const handleEmailType = async (value) => {
+    try {
+      if (!value) return toast.error("Please select the email type!");
+      setModalVisible(false);
+      const res = await api.patch(
+        `/updateBadge/${persistedUserInfo._id}/${persistedUserInfo.badges[0]._id}`,
+        {
+          type: value,
+        },
+      );
+      if (res.status === 200) {
+        localStorage.setItem("uId", res.data.uuid);
+        localStorage.setItem("userLoggedIn", res.data.uuid);
+        localStorage.removeItem("isGuestMode");
+        localStorage.setItem("jwt", res.data.token);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      toast.error(error.response.data.message.split(":")[1]);
+    }
+  };
+
   return (
-    <div className="no-scrollbar hidden h-[calc(100vh-96px)] w-[23rem] min-w-[23rem] overflow-y-auto bg-white pl-[1.3rem] pr-[2.1rem] pt-[4vh] shadow-[0_3px_10px_rgb(0,0,0,0.2)] 2xl:w-[25rem] laptop:block dark:border-l-2 dark:border-white dark:bg-[#0A0A0C]">
-      <div className="mb-[5vh] flex gap-6">
-        <img src="/assets/svgs/dashboard/treasure.svg" alt="badge" />
-        <div>
-          <h4 className="heading">Treasury</h4>
-          <p className="whitespace-nowrap text-[20px] font-medium text-[#616161] dark:text-white">
-            Balance: <span>{treasuryAmount}</span>
-          </p>
-        </div>
-      </div>
-      {localStorage.getItem("isGuestMode") ? (
-        <div className="mb-[5vh] flex items-center gap-6">
-          <div className="relative h-fit w-fit">
-            <img src="/assets/svgs/dashboard/guestBadge.svg" alt="badge" />
-            <p className="transform-center absolute z-50 pb-5 text-[1.875rem] font-medium leading-normal text-[#362E04]">
-              G
+    <>
+      <div className="no-scrollbar hidden h-[calc(100vh-96px)] w-[23rem] min-w-[23rem] overflow-y-auto bg-white pl-[1.3rem] pr-[2.1rem] pt-[4vh] shadow-[0_3px_10px_rgb(0,0,0,0.2)] 2xl:w-[25rem] laptop:block dark:border-l-2 dark:border-white dark:bg-[#0A0A0C]">
+        <EmailTypeModal 
+          modalShow={modalVisible}
+          email={persistedUserInfo?.email}
+          handleEmailType={handleEmailType}
+        />
+        <div className="mb-[5vh] flex gap-6">
+          <img src="/assets/svgs/dashboard/treasure.svg" alt="badge" />
+          <div>
+            <h4 className="heading">Treasury</h4>
+            <p className="whitespace-nowrap text-[20px] font-medium text-[#616161] dark:text-white">
+              Balance: <span>{treasuryAmount}</span>
             </p>
           </div>
-          <div>
-            <h4 className="heading">Guest User</h4>
-            <div className="font-inter mt-[-4px] flex gap-1 text-[10.79px] text-base  font-medium text-[#616161] tablet:text-[17px] laptop:text-[20px] dark:text-[#D2D2D2]">
-              <p>Balance: </p>
-              <p>
-                {persistedUserInfo?.balance
-                  ? persistedUserInfo?.balance.toFixed(2)
-                  : 0}
-              </p>
-            </div>
-            <div
-              onClick={() => {
-                navigate("/signup");
-              }}
-            >
-              <Anchor className="cursor-pointer text-[#4A8DBD] dark:text-[#BAE2FF]">
-                Create Account
-              </Anchor>
-            </div>
-          </div>
         </div>
-      ) : (
-        <div
-          className="mb-[5vh] flex items-center gap-6"
-          onClick={() => {
-            navigate("/profile");
-          }}
-        >
-          <div className="relative h-fit w-fit">
-            <img
-              src="/assets/svgs/dashboard/MeBadge.svg"
-              alt="badge"
-              className="tablet:h-[5.43rem] tablet:w-[4.36rem]"
-            />
-            <p className="transform-center absolute z-50 pb-5 text-[35px] font-bold leading-normal text-[#7A7016]">
-              5
-            </p>
-          </div>
-          <div>
-            <h4 className="heading">My Profile</h4>
-            <div className="font-inter mt-[-4px] flex gap-1 text-[10.79px] text-base  font-medium text-[#616161] tablet:text-[17px] laptop:text-[20px] dark:text-[#D2D2D2]">
-              <p>Balance: </p>
-              <p>
-                {persistedUserInfo?.balance
-                  ? persistedUserInfo?.balance.toFixed(2)
-                  : 0}
+        {localStorage.getItem("isGuestMode") ? (
+          <div className="mb-[5vh] flex items-center gap-6">
+            <div className="relative h-fit w-fit">
+              <img src="/assets/svgs/dashboard/guestBadge.svg" alt="badge" />
+              <p className="transform-center absolute z-50 pb-5 text-[1.875rem] font-medium leading-normal text-[#362E04]">
+                G
               </p>
             </div>
             <div>
-              <Anchor className="cursor-pointer text-[#4A8DBD] dark:text-[#BAE2FF]">
-                Edit Profile
-              </Anchor>
-            </div>
-            <div className="mt-3 flex gap-2">
-              <div className="h-[9px] w-6 rounded-md bg-[#4A8DBD]"></div>
-              <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
-              <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
-              <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
-              <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
-              <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
+              <h4 className="heading">Guest User</h4>
+              <div className="font-inter mt-[-4px] flex gap-1 text-[10.79px] text-base  font-medium text-[#616161] tablet:text-[17px] laptop:text-[20px] dark:text-[#D2D2D2]">
+                <p>Balance: </p>
+                <p>
+                  {persistedUserInfo?.balance
+                    ? persistedUserInfo?.balance.toFixed(2)
+                    : 0}
+                </p>
+              </div>
+              <div
+                onClick={() => {
+                  navigate("/signup");
+                }}
+              >
+                <Anchor className="cursor-pointer text-[#4A8DBD] dark:text-[#BAE2FF]">
+                  Create Account
+                </Anchor>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      {sidebarList.map((item) => (
+        ) : (
+          <div
+            className="mb-[5vh] flex items-center gap-6"
+            onClick={() => {
+              navigate("/profile");
+            }}
+          >
+            <div className="relative h-fit w-fit">
+              <img
+                src="/assets/svgs/dashboard/MeBadge.svg"
+                alt="badge"
+                className="tablet:h-[5.43rem] tablet:w-[4.36rem]"
+              />
+              <p className="transform-center absolute z-50 pb-5 text-[35px] font-bold leading-normal text-[#7A7016]">
+                5
+              </p>
+            </div>
+            <div>
+              <h4 className="heading">My Profile</h4>
+              <div className="font-inter mt-[-4px] flex gap-1 text-[10.79px] text-base  font-medium text-[#616161] tablet:text-[17px] laptop:text-[20px] dark:text-[#D2D2D2]">
+                <p>Balance: </p>
+                <p>
+                  {persistedUserInfo?.balance
+                    ? persistedUserInfo?.balance.toFixed(2)
+                    : 0}
+                </p>
+              </div>
+              <div>
+                <Anchor className="cursor-pointer text-[#4A8DBD] dark:text-[#BAE2FF]">
+                  Edit Profile
+                </Anchor>
+              </div>
+              <div className="mt-3 flex gap-2">
+                <div className="h-[9px] w-6 rounded-md bg-[#4A8DBD]"></div>
+                <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
+                <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
+                <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
+                <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
+                <div className="h-[9px] w-6 rounded-md bg-[#D9D9D9] dark:bg-[#323232]"></div>
+              </div>
+            </div>
+          </div>
+        )}
+        {sidebarList.map((item) => (
         <div className="mt-[1.9vh] flex items-center gap-4" key={item.id}>
           {persistedTheme === "dark" ? (
             <img src={item.icon} alt={item.alt} />
@@ -247,13 +282,14 @@ const SidebarRight = () => {
             <img src={item.iconLight} alt={item.alt} />
           )}
 
-          <div className="flex w-full items-center justify-between text-[18px] font-semibold leading-normal text-[#7C7C7C] dark:text-[#878787]">
-            <h5>{item.title}</h5>
-            <h5>{item.value}</h5>
+            <div className="flex w-full items-center justify-between text-[18px] font-semibold leading-normal text-[#7C7C7C] dark:text-[#878787]">
+              <h5>{item.title}</h5>
+              <h5>{item.value}</h5>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
