e5b7e035281ff792063eac834c6e351f8b323334
Hamzazaheer1 <mhamzazaheer1@gmail.com>
2024-01-22 12:22:45 +0500

    Added padding in create-quest

--- src/pages/Dashboard/pages/Quest/Quest.jsx
+++ src/pages/Dashboard/pages/Quest/Quest.jsx
@@ -12,13 +12,13 @@ const Quest = () => {
   const handleTab = (id) => setTab(id);
 
   return (
-    <div className="h-[calc(100vh-96px)] w-full overflow-y-auto bg-white pt-[20px] tablet:pt-[60px] dark:bg-[#0A0A0C] tablet:h-[calc(100vh-116px)] laptop:h-[calc(100vh-96px)]">
+    <div className="h-[calc(100vh-96px)] w-full overflow-y-auto bg-white py-[20px] dark:bg-[#0A0A0C] tablet:h-[calc(100vh-116px)] tablet:py-[60px] laptop:h-[calc(100vh-96px)]">
       <Navbar handleTab={handleTab} tab={tab} />
       {tab === 0 && <RankChoice />}
       {tab === 1 && <MultipleChoice />}
       {tab === 2 && <AgreeDisagree />}
       {tab === 3 && <YesNo />}
-      {tab === 4 && <LikeDislike/>}
+      {tab === 4 && <LikeDislike />}
     </div>
   );
 };
