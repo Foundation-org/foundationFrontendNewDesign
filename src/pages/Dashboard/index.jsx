557c167985af13e2debdf46b985fc63142a62daa
Hamzazaheer1 <mhamzazaheer1@gmail.com>
2024-01-26 11:14:27 +0500

    test2

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,65 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Topbar from './components/Topbar';
-import SidebarLeft from './components/SidebarLeft';
-import { useEffect, useState } from 'react';
-
-import * as QuestServices from '../../services/queries/quest';
-import * as prefActions from '../../features/preferences/prefSlice';
-import { initialColumns } from '../../constants/preferences';
-import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
-  // preferences start
-  const [columns, setColumns] = useState(initialColumns);
-  const getPreferences = useSelector(prefActions.getPrefs);
-  const { data: topicsData, isSuccess } = QuestServices.useGetAllTopics();
-
-  const { data: prefSearchRes } = QuestServices.useSearchTopics(getPreferences);
-
-  useEffect(() => {
-    if (prefSearchRes?.length !== 0) {
-      setColumns((prevColumns) => {
-        const newList = prefSearchRes?.data.data || [];
-
-        const filteredList = newList.filter(
-          (item) => !prevColumns.Block.list.includes(item) && !prevColumns.Preferences.list.includes(item),
-        );
-
-        return {
-          ...prevColumns,
-          All: {
-            ...prevColumns.All,
-            list: filteredList || [],
-          },
-        };
-      });
-    } else {
-      if (isSuccess) {
-        setColumns((prevColumns) => {
-          const newList = topicsData?.data.data || [];
-
-          const filteredList = newList.filter(
-            (item) => !prevColumns.Block.list.includes(item) && !prevColumns.Preferences.list.includes(item),
-          );
-
-          return {
-            ...prevColumns,
-            All: {
-              ...prevColumns.All,
-              list: filteredList || [],
-            },
-          };
-        });
-      }
-    }
-  }, [topicsData, prefSearchRes]);
-  // preferences end
-
   return (
     <>
-      <Topbar />
-      <SidebarLeft columns={columns} setColumns={setColumns} />
-      <div className="flex h-[calc(100vh-183.8px)] justify-between">
+      {/* <Topbar /> */}
+      {/* h-[calc(100vh-66.8px)] */}
+      <div className="flex h-screen justify-between">
         <Outlet />
       </div>
     </>
