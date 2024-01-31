be34e6d6d149b1740aba81934cdd743f9bb2ce3e
Muhammad Zubair <93026937+izubaire@users.noreply.github.com>
2024-01-31 15:24:31 +0500

    unverification badges grayed out

--- src/pages/Dashboard/pages/Profile/components/Button.jsx
+++ src/pages/Dashboard/pages/Profile/components/Button.jsx
@@ -6,6 +6,7 @@ const Button = ({ children, type = 'button', color, onClick }) => {
     {
       '  text-white bg-[#FAD308]   rounded-[6px] tablet:rounded-[12.6px] laptop:rounded-[23px]': color === 'yellow',
       'border-[1px] border-[#BABABA] text-[#949494] dark-bg-[#252D37] bg-[#fff] rounded-[23px]': color === 'gray-light',
+      'bg-gray cursor-not-allowed border-[1px] border-[#BABABA] text-white': color === 'gray',
       'bg-[#FF4057] dark:bg-[#C13232] text-white rounded-[6px] tablet:rounded-[12.6px] laptop:rounded-[23px]':
         color === 'red',
       'bg-gradient-to-r from-[#6BA5CF] to-[#389CE3] dark:bg-[#252D37] dark:from-[#252D37] dark:to-[#252D37] text-white rounded-[6px] tablet:rounded-[12.6px] laptop:rounded-[23px]':
