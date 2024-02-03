58edad112f1f0a80dcaee16980faf9d4e8f0228f
Hamzazaheer1 <mhamzazaheer1@gmail.com>
2024-02-03 17:13:48 +0500

    term of services

--- src/pages/Signup/components/TermsPrivacyLayout.jsx
+++ src/pages/Signup/components/TermsPrivacyLayout.jsx
@@ -1,10 +1,10 @@
 const TermsPrivacyLayout = ({ children }) => {
   return (
-    <div className="bg-[#F3F3F3] min-h-screen h-full">
+    <div className="bg-[#F3F3F3] h-full">
       <div className="bg-blue-gradiant h-[194px] flex items-center justify-center">
         <img src="/assets/svgs/logo.svg" alt="logo" className="w-[34.5px] tablet:w-[69.2px] laptop:w-[5.75rem]" />
       </div>
-      <div className="bg-white w-[90%] mx-auto rounded-[26px] border-[3px] border-[#DEE6F7] text-[#707175]">
+      <div className="bg-white w-[90%] mx-auto rounded-[26px] border-[3px] border-[#DEE6F7] text-[#707175] h-full overflow-y-auto mt-[-2.5rem] custom-scrollbar pb-[2rem]">
         {children}
       </div>
     </div>
