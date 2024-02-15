89736e67bdb3aef4ce0c6b8165f25a81a95898eb
Muhammad Zubair <93026937+izubaire@users.noreply.github.com>
2024-02-16 04:17:46 +0500

    verification popup working

--- src/pages/Dashboard/pages/Profile/components/VerificationPopups.jsx
+++ src/pages/Dashboard/pages/Profile/components/VerificationPopups.jsx
@@ -1,13 +1,55 @@
 import { LoginSocialGoogle } from 'reactjs-social-login';
 import PopUp from '../../../../../components/ui/PopUp';
 import { Button } from '../../../../../components/ui/Button';
+import api from '../../../../../services/api/Axios';
+import { toast } from 'sonner';
+import { useState } from 'react';
 
 const REDIRECT_URI = window.location.href;
-
-const VerificationPopups = ({ isPopup, setIsPopup, title, logo, placeholder }) => {
+const VerificationPopups = ({ isPopup, setIsPopup, title, logo, placeholder, selectedBadge, handleUserInfo }) => {
+    const [email, setEmail] = useState('')
   const handleClose = () => {
     setIsPopup(false);
   };
+
+    // Handle Add Contact Badge
+    const handleAddContactBadge = async ({provider, data, legacy}) => {
+      try {
+        let addBadge;
+        if(legacy) {
+          if(email === "") return toast.warning("Please enter your email address")
+          addBadge = await api.post(`/addBadge/contact`, {
+            legacy,
+            email,
+            uuid: localStorage.getItem('uuid'),
+            type: selectedBadge,
+          });
+        } else {
+          data['provider'] = provider;
+          data['type'] = selectedBadge;
+          data['uuid'] = localStorage.getItem('uuid');
+          addBadge = await api.post(`/addBadge/contact`, {
+            ...data,
+          });
+        }
+        if (addBadge.status === 200) {
+          toast.success('Badge Added Successfully!');
+          handleClose()
+          handleUserInfo();
+          setEmail("")
+        }
+        if (addBadge.status === 201) {
+          toast.success('Email Send Successfully!');
+          handleClose()
+          handleUserInfo();
+          setEmail("")
+        }
+      } catch (error) {
+        toast.error(error.response.data.message.split(':')[1]);
+        handleClose()
+        setEmail("");
+      }
+    };
 
   return (
     <div>
@@ -25,6 +67,7 @@ const VerificationPopups = ({ isPopup, setIsPopup, title, logo, placeholder }) =
               //   setProfile(data);
               //   data['provider'] = provider;
               //   isLogin ? handleSignInSocial(data) : handleSignUpSocialAfterReferral(data);
+              handleAddContactBadge({provider, data})
             }}
             onReject={(err) => {
               console.log(err);
@@ -51,10 +94,12 @@ const VerificationPopups = ({ isPopup, setIsPopup, title, logo, placeholder }) =
             </p>
             <input
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder={placeholder}
               className="w-full text-[9.28px] tablet:text-[18px] leading-[11.23px] tablet:leading-[21px] font-medium text-[#B6B4B4] border tablet:border-[3px] border-[#DEE6F7] bg-[#FBFBFB] rounded-[8.62px] tablet:rounded-[15px] py-2 tablet:py-[18px] px-[16px] focus:outline-none mt-1 tablet:mt-[15px] mb-[10px] tablet:mb-5"
             />
-            <div className="flex justify-end">
+            <div className="flex justify-end" onClick={() => handleAddContactBadge({legacy: true})}>
               <Button variant="submit">Verify Email</Button>
             </div>
           </div>
