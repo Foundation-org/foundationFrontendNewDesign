d838d3925410455fc1e6b603b30a85528bd1be4f
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-02-17 19:55:04 +0500

    verification badges new desgin

--- src/pages/Dashboard/pages/Profile/components/Button.jsx
+++ src/pages/Dashboard/pages/Profile/components/Button.jsx
@@ -2,14 +2,13 @@ import classNames from 'classnames';
 
 const Button = ({ children, type = 'button', color, onClick, disabled }) => {
   const buttonClasses = classNames(
-    'w-[28.3vw] tablet:w-[19.9vw] h-[7.3vw] tablet:h-[5.8vw] rounded-[1.31vw] text-[2.65vw] tablet:text-[1.73vw] font-semibold leading-normal tablet:mr-[18.5px]',
+    'w-[28.3vw] h-[7.3vw] tablet:w-[17.36vw] tablet:h-[3.48vw] rounded-[1.31vw] tablet:rounded-[8px] laptop:rounded-[15px] text-[2.65vw] tablet:text-[1.38vw] font-semibold leading-normal',
     {
-      'text-white bg-[#FAD308] rounded-[6px] tablet:rounded-[12.6px] laptop:rounded-[23px]': color === 'yellow',
-      'border-[1px] border-[#BABABA] text-[#949494] dark-bg-[#252D37] bg-[#fff] rounded-[23px]': color === 'gray-light',
+      'text-white bg-[#FAD308]': color === 'yellow',
+      'border-[1px] border-[#BABABA] text-[#949494] dark-bg-[#252D37] bg-[#fff]': color === 'gray-light',
       'bg-gray cursor-not-allowed border-[1px] border-[#BABABA] text-white': color === 'gray',
-      'bg-[#FF4057] dark:bg-[#C13232] text-white rounded-[6px] tablet:rounded-[12.6px] laptop:rounded-[23px]':
-        color === 'red',
-      'bg-gradient-to-r from-[#6BA5CF] to-[#389CE3] dark:bg-[#252D37] dark:from-[#252D37] dark:to-[#252D37] text-white rounded-[6px] tablet:rounded-[12.6px] laptop:rounded-[23px]':
+      'bg-[#FF4057] dark:bg-[#C13232] text-white': color === 'red',
+      'bg-gradient-to-r from-[#6BA5CF] to-[#389CE3] dark:bg-[#252D37] dark:from-[#252D37] dark:to-[#252D37] text-white':
         color === 'blue',
     },
   );
