da15bc4786c84b78771061f47e6ce5db849dae9c
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-03-06 18:33:02 +0500

    Fixed going back and forth between hidden and shared its populating the wrong feed briefly

--- src/pages/Dashboard/pages/QuestStartSection/index.jsx
+++ src/pages/Dashboard/pages/QuestStartSection/index.jsx
@@ -264,14 +264,14 @@ const QuestStartSection = () => {
   // );
 
   return (
-    <div className="flex w-full flex-col bg-white dark:bg-black laptop:flex-row">
+    <div className="flex w-full flex-col bg-white laptop:flex-row dark:bg-black">
       <SidebarLeft
         columns={columns}
         setColumns={setColumns}
         itemsWithCross={itemsWithCross}
         setItemsWithCross={setItemsWithCross}
       />
-      <div className="no-scrollbar flex h-full w-full flex-col overflow-y-auto bg-[#F3F3F3] px-[1.13rem] py-[0.63rem] dark:bg-[#242424] tablet:min-h-[calc(100vh-92px)] tablet:py-[0.94rem]">
+      <div className="no-scrollbar flex h-full w-full flex-col overflow-y-auto bg-[#F3F3F3] px-[1.13rem] py-[0.63rem] tablet:min-h-[calc(100vh-92px)] tablet:py-[0.94rem] dark:bg-[#242424]">
         <InfiniteScroll
           dataLength={allData?.length}
           next={fetchMoreData}
