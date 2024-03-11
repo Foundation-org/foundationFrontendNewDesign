094adfcec70a9341e8a0b93f773901fa12bc4fd7
Wamiq <wamiqakram@gmail.com>
2024-03-11 19:50:31 +0500

    to fixed removed and loading added in twitter

--- src/pages/Dashboard/components/SidebarRight.jsx
+++ src/pages/Dashboard/components/SidebarRight.jsx
@@ -261,7 +261,7 @@ const SidebarRight = () => {
           <div>
             <h4 className="heading">Treasury</h4>
             <p className="whitespace-nowrap text-[20px] font-medium text-[#616161] dark:text-white">
-              <span>{treasuryAmount?.toFixed(2)} FDX</span>
+              <span>{treasuryAmount} FDX</span>
             </p>
           </div>
         </div>
