03c0838cc2721c98ead81ffae0d0ae167ec3e9c9
Wamiq <wamiqakram@gmail.com>
2024-03-12 20:10:47 +0500

    params of insta changed

--- src/pages/Dashboard/pages/Profile/pages/VerificationBadges.jsx
+++ src/pages/Dashboard/pages/Profile/pages/VerificationBadges.jsx
@@ -621,7 +621,8 @@ const VerificationBadges = () => {
                     onFailure={(err) => console.log('error', err)}
                     redirectUri={window.location.href}
                     cssClass={'hideBack'}
-                    scope='user_profile,user_media,instagram_graph_user_profile'
+                    implicitAuth={true}
+                    scope="user_profile"
                   >
                     <Button color={checkSocial('instagram') ? 'red' : 'blue'}>
                       {checkSocial('instagram') ? '' : 'Add New Badge'}
