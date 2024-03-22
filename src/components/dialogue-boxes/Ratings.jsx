cf40087a60869fae162acad8e5a981cf7821bd51
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-03-22 21:10:53 +0500

    Fixed ratings icons

--- src/components/dialogue-boxes/Ratings.jsx
+++ src/components/dialogue-boxes/Ratings.jsx
@@ -66,38 +66,38 @@ export default function Ratings({ handleClose, modalVisible, questStartData }) {
           />
           <div className="mt-1 flex justify-between tablet:mt-[18px]">
             {rangeVal.map((item, index) => (
-              <div key={index + 1}>
+              <div key={index + 1} className="relative">
                 <p className="text-[8px] font-semibold leading-normal text-[#9E9E9E] tablet:text-[18px]">{item}</p>
                 {item === 0 && (
-                  <p className="-ml-1 mt-[6px] flex h-[13px] w-[14px] items-center justify-center rounded-[1.56px] border-[0.52px] border-[#0F5634] bg-[#0FB063] text-[10.4px] font-normal leading-[1px] text-white tablet:-ml-[10px] tablet:mt-3 tablet:h-[35px] tablet:w-[35px] tablet:rounded-[4.2px] tablet:border-[1.4px] tablet:text-[28px]">
-                    G
-                  </p>
-                )}
-                {item === 20 && (
-                  <p className="-ml-1 mt-[6px] flex h-[13px] w-[17px] items-center justify-center rounded-[1.56px] border-[0.52px] border-[#9F9F4E] bg-[#F7F71E] text-[10.4px] font-normal leading-[1px] text-[#4A472B] tablet:-ml-[10px] tablet:mt-3 tablet:h-[35px] tablet:w-[46.2px] tablet:rounded-[4.2px] tablet:border-[1.4px] tablet:text-[28px]">
-                    PG
-                  </p>
-                )}
+                  <div className="absolute left-1/2 top-[15px] w-[13px] -translate-x-1/2 transform tablet:top-[36px] tablet:w-[35px]">
+                    <img src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/ratings/desk-g.svg`} alt="desk-g" />
+                  </div>
+                )}{' '}
+                <div className="absolute left-1/2 top-[15px] w-[20px] -translate-x-1/2 transform tablet:top-[36px] tablet:w-[50px]">
+                  {item === 20 && (
+                    <img src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/ratings/desk-pg.svg`} alt="desk-pg" />
+                  )}
+                </div>
                 {item === 40 && (
-                  <p className="-ml-[2px] mt-[6px] flex h-[13px] w-[14px] items-center justify-center rounded-[1.56px] border-[0.52px] border-[#1E1111] bg-[#DB0000] text-[10.4px] font-normal leading-[1px] text-white tablet:-ml-[4px] tablet:mt-3 tablet:h-[35px] tablet:w-[35px] tablet:rounded-[4.2px] tablet:border-[1.4px] tablet:text-[28px]">
-                    R
-                  </p>
+                  <div className="absolute left-1/2 top-[15px] w-[13px] -translate-x-1/2 transform tablet:top-[36px] tablet:w-[35px]">
+                    <img src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/ratings/desk-r.svg`} alt="desk-r" />
+                  </div>
                 )}
                 {item === 60 && (
-                  <p className="-ml-1 mt-[6px] flex h-[13px] w-[19.5px] items-center justify-center rounded-[1.56px] border-[0.52px] border-[#0670AB] bg-[#26B8F6] text-[10.4px] font-normal leading-[1px] text-white  tablet:-ml-[10px] tablet:mt-3 tablet:h-[35px] tablet:w-[52.5px] tablet:rounded-[4.2px] tablet:border-[1.4px] tablet:text-[28px]">
-                    NC
-                  </p>
+                  <div className="absolute left-1/2 top-[15px] w-[20px] -translate-x-1/2 transform tablet:top-[36px] tablet:w-[50px]">
+                    <img src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/ratings/desk-nc.svg`} alt="desk-nc" />
+                  </div>
                 )}
                 {item === 80 && (
-                  <p className="-ml-[2px] mt-[6px] flex h-[13px] w-[14px] items-center justify-center rounded-[1.56px] border-[0.52px] border-[#696969] bg-[#0F0F0F] text-[10.4px] font-normal leading-[1px] text-white tablet:mt-3 tablet:h-[35px] tablet:w-[35px] tablet:rounded-[4.2px] tablet:border-[1.4px] tablet:text-[28px]">
-                    X
-                  </p>
+                  <div className="absolute left-1/2 top-[15px] w-[13px]  -translate-x-1/2 transform tablet:top-[36px] tablet:w-[35px]">
+                    <img src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/ratings/desk-x.svg`} alt="desk-x" />
+                  </div>
                 )}
               </div>
             ))}
           </div>
         </div>
-        <div className="mt-[10px] flex justify-end gap-[15px] tablet:mt-[25px] tablet:gap-[34px]">
+        <div className="mt-7 flex justify-end gap-[15px] tablet:mt-[75px] tablet:gap-[34px]">
           <Button variant={'danger'} onClick={handleClose}>
             Cancel
           </Button>
