3852ba04e0fc0027526cc3db344baab1f382a67b
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-03-23 17:43:14 +0500

    Fixed drag to top not checking

--- src/pages/Dashboard/pages/Quest/pages/MultipleChoice.jsx
+++ src/pages/Dashboard/pages/Quest/pages/MultipleChoice.jsx
@@ -130,7 +130,11 @@ const MultipleChoice = () => {
     }
   };
 
-  const answerVerification = async (id, index, value) => {
+  const answerVerification = async (id, index, value, extra) => {
+    if (extra) {
+      if (extra === value) return;
+    }
+
     if (optionsValue[index].chatgptQuestion === value) return;
 
     dispatch(createQuestAction.checkAnswer({ id, value, index }));
@@ -148,6 +152,12 @@ const MultipleChoice = () => {
     if (!result.destination) {
       return;
     }
+    answerVerification(
+      `index-${result.destination.index}`,
+      result.destination.index,
+      optionsValue[parseInt(result.draggableId.split('-')[1])].question,
+      optionsValue[parseInt(result.draggableId.split('-')[1])].chatgptQuestion,
+    );
 
     const newTypedValues = [...optionsValue];
     const [removed] = newTypedValues.splice(result.source.index, 1);
--- src/pages/Dashboard/pages/Quest/pages/OpenChoice.jsx
+++ src/pages/Dashboard/pages/Quest/pages/OpenChoice.jsx
@@ -128,7 +128,11 @@ const OpenChoice = () => {
     }
   };
 
-  const answerVerification = async (id, index, value) => {
+  const answerVerification = async (id, index, value, extra) => {
+    if (extra) {
+      if (extra === value) return;
+    }
+
     if (optionsValue[index].chatgptQuestion === value) return;
 
     dispatch(createQuestAction.checkAnswer({ id, value, index }));
@@ -146,6 +150,13 @@ const OpenChoice = () => {
     if (!result.destination) {
       return;
     }
+
+    answerVerification(
+      `index-${result.destination.index}`,
+      result.destination.index,
+      optionsValue[parseInt(result.draggableId.split('-')[1])].question,
+      optionsValue[parseInt(result.draggableId.split('-')[1])].chatgptQuestion,
+    );
 
     const newTypedValues = [...optionsValue];
     const [removed] = newTypedValues.splice(result.source.index, 1);
