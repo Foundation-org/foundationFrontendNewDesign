9c2d4424156a758234abd94cbab26986629b1e23
Wamiq <wamiqakram@gmail.com>
2024-03-29 07:05:48 +0500

    card topbar badge  text size fixed

--- src/components/question-card/CardTopbar.jsx
+++ src/components/question-card/CardTopbar.jsx
@@ -46,7 +46,7 @@ const CardTopbar = ({
               alt={alt}
               className="h-[18.5px] w-[14.6px] tablet:h-[40.714px] tablet:w-[32.134px] laptop:h-[29px] laptop:w-[22.888px]"
             />
-            <p className="absolute left-1/2 top-[41%] z-50 -translate-x-1/2 -translate-y-1/2 text-[7.3px] font-normal leading-none text-[#7A7016] tablet:top-[40%] tablet:text-[12px]">
+            <p className="absolute left-1/2 top-[41%] z-50 -translate-x-1/2 -translate-y-1/2 text-[7.3px] font-normal leading-none text-[#7A7016] tablet:top-[40%] tablet:text-[13px]">
               {badgeCount}
             </p>
           </div>
@@ -57,7 +57,7 @@ const CardTopbar = ({
               alt={alt}
               className="h-[18.5px] w-[14.6px] tablet:h-[40.714px] tablet:w-[32.134px] laptop:h-[29px] laptop:w-[22.888px]"
             />
-            <p className="absolute left-1/2 top-[41%] z-50 -translate-x-1/2 -translate-y-1/2 text-[7.3px] font-normal leading-none text-[#F6F6F6] tablet:top-[40%] tablet:text-[15px]">
+            <p className="absolute left-1/2 top-[41%] z-50 -translate-x-1/2 -translate-y-1/2 text-[7.3px] font-normal leading-none text-[#F6F6F6] tablet:top-[40%] tablet:text-[13px]">
               {badgeCount}
             </p>
           </div>
