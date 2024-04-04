4065eb79fb7c6f553d554a3582bfb14cb46cd075
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-04-04 23:57:22 +0500

    placeholder updated

--- src/pages/Dashboard/pages/Quest/components/AddMedia.jsx
+++ src/pages/Dashboard/pages/Quest/components/AddMedia.jsx
@@ -124,7 +124,7 @@ export default function AddMedia({ handleTab }) {
               onChange={handleDescChange}
               onBlur={(e) => e.target.value.trim() !== '' && descVerification(e.target.value.trim())}
               value={getMediaStates.desctiption}
-              placeholder="Please decribe this embeded video....."
+              placeholder="Please describe this media..."
               onKeyDown={(e) => e.key === 'Tab' || (e.key === 'Enter' && handleTab(0, 'Enter'))}
               className="w-full resize-none rounded-l-[5.128px] border-y border-l border-[#DEE6F7] bg-white px-[9.24px] pb-2 pt-[7px] text-[0.625rem] font-medium leading-[13px] text-[#7C7C7C] focus-visible:outline-none tablet:rounded-l-[10.3px] tablet:border-y-[3px] tablet:border-l-[3px] tablet:px-[18px] tablet:py-[11.6px] tablet:text-[1.296rem] tablet:leading-[23px] laptop:rounded-l-[0.625rem] laptop:py-[13px] laptop:text-[1.25rem] dark:border-[#0D1012] dark:bg-[#0D1012] dark:text-[#7C7C7C]"
             />
