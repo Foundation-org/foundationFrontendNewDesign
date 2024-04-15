63dc85fc1f679b6469968419867f4d6396676077
Wamiq <wamiqakram@gmail.com>
2024-04-15 10:53:17 +0500

    return to pause video

--- src/components/question-card/EmbededVideo.jsx
+++ src/components/question-card/EmbededVideo.jsx
@@ -36,15 +36,14 @@ export const EmbededVideo = ({ description, url, setPlayingPlayerId, questId, pl
 
   return (
     <div
-      className={`align-items mx-[22px] mb-2 flex flex-col justify-start rounded-[9.183px] border border-[#DEE6F7] px-[5px] py-2 tablet:mx-[60px] tablet:mb-[14px] tablet:border-[2.755px] tablet:px-2 ${
-        url
+      className={`align-items mx-[22px] mb-2 flex flex-col justify-start rounded-[9.183px] border border-[#DEE6F7] px-[5px] py-2 tablet:mx-[60px] tablet:mb-[14px] tablet:border-[2.755px] tablet:px-2 ${url
           ? youtubeBaseURLs.some((baseURL) => url.includes(baseURL))
             ? 'h-[169px] tablet:h-[420px]'
             : url.includes(soundcloudUnique)
               ? 'h-[162px] tablet:h-[226px]'
               : ''
           : ''
-      }`}
+        }`}
     >
       <h2 className="mb-1 ml-[9px] text-[8px] font-medium text-[#7C7C7C] tablet:text-[14.692px]">{description}</h2>
       <div className="">
@@ -63,7 +62,7 @@ export const EmbededVideo = ({ description, url, setPlayingPlayerId, questId, pl
           }}
           width="100%"
           height="100%"
-          onPause={() => playing && setPlayingPlayerId('')}
+          // onPause={() => playing && setPlayingPlayerId('')}
           // single_active={true}
           controls={true} // Hide player controls
           muted={false} // Unmute audio
