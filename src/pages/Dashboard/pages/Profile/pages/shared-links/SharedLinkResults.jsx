fd0049dc17d2292d9fad338b7c59e2f161003f42
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-04-25 16:15:41 +0500

    changes

--- src/pages/Dashboard/pages/Profile/pages/shared-links/SharedLinkResults.jsx
+++ src/pages/Dashboard/pages/Profile/pages/shared-links/SharedLinkResults.jsx
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
 import Loader from '../../../../../../components/ui/Loader';
-import SidebarRight from '../../../../components/SidebarRight';
 import Topbar from '../../../../components/Topbar';
 import QuestionCardWithToggle from '../../../QuestStartSection/components/QuestionCardWithToggle';
 import { getQuestById } from '../../../../../../services/api/homepageApis';
@@ -12,7 +11,7 @@ import DashboardLayout from '../../../../components/DashboardLayout';
 export default function SharedLinkResults() {
   const location = useLocation();
   const persistedUserInfo = useSelector((state) => state.auth.user);
-  const [tab, setTab] = useState('My Link Results');
+  const [tab, setTab] = useState(persistedUserInfo.role === 'guest' ? 'All Results' : 'My Link Results');
   const [questData, setQuestData] = useState();
   const [allQuestData, setAllQuestData] = useState();
   const [loading, setLoading] = useState(false);
@@ -61,28 +60,30 @@ export default function SharedLinkResults() {
       <div className="bg-[#F2F3F5] dark:bg-[#242424]">
         <DashboardLayout>
           <div className="no-scrollbar h-[calc(100dvh-101px)] w-full overflow-y-auto tablet:h-[calc(100vh-70px)]">
-            <div className="mb-[10px] mt-[15px] flex justify-center gap-[15px] tablet:gap-5 laptop:mb-[37px] laptop:mt-[43px] laptop:gap-[35px]">
-              <button
-                className={`${
-                  tab === 'All Results'
-                    ? 'border-[#4A8DBD] bg-[#4A8DBD] text-white dark:border-[#252D37] dark:bg-white dark:text-black'
-                    : 'border-[#BABABA] bg-[#f9f9f9] text-[#7C7C7C] dark:bg-[#212428]'
-                } tab-button`}
-                onClick={() => setTab('All Results')}
-              >
-                All Results
-              </button>
-              <button
-                className={`${
-                  tab === 'My Link Results'
-                    ? 'border-[#4A8DBD] bg-[#4A8DBD] text-white dark:border-[#252D37] dark:bg-white dark:text-black'
-                    : 'border-[#BABABA] bg-[#f9f9f9] text-[#7C7C7C] dark:bg-[#212428]'
-                } tab-button`}
-                onClick={() => setTab('My Link Results')}
-              >
-                My Link Results
-              </button>
-            </div>
+            {persistedUserInfo?.role === 'user' && (
+              <div className="mb-[10px] mt-[15px] flex justify-center gap-[15px] tablet:gap-5 laptop:mb-[37px] laptop:mt-[43px] laptop:gap-[35px]">
+                <button
+                  className={`${
+                    tab === 'All Results'
+                      ? 'border-[#4A8DBD] bg-[#4A8DBD] text-white dark:border-[#252D37] dark:bg-white dark:text-black'
+                      : 'border-[#BABABA] bg-[#f9f9f9] text-[#7C7C7C] dark:bg-[#212428]'
+                  } tab-button`}
+                  onClick={() => setTab('All Results')}
+                >
+                  All Results
+                </button>
+                <button
+                  className={`${
+                    tab === 'My Link Results'
+                      ? 'border-[#4A8DBD] bg-[#4A8DBD] text-white dark:border-[#252D37] dark:bg-white dark:text-black'
+                      : 'border-[#BABABA] bg-[#f9f9f9] text-[#7C7C7C] dark:bg-[#212428]'
+                  } tab-button`}
+                  onClick={() => setTab('My Link Results')}
+                >
+                  My Link Results
+                </button>
+              </div>
+            )}
 
             {loading ? (
               <Loader />
@@ -98,7 +99,9 @@ export default function SharedLinkResults() {
               )
             ) : (
               allQuestData && (
-                <div className="mx-auto px-4 tablet:px-6 laptop:max-w-[730px] laptop:px-[0px]">
+                <div
+                  className={`${persistedUserInfo?.role === 'guest' ? 'pt-5' : ''} mx-auto px-4 tablet:max-w-[730px] tablet:px-6 laptop:px-[0px]`}
+                >
                   <QuestionCardWithToggle
                     questStartData={allQuestData}
                     postProperties={'actual-results'}
