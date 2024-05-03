779065f2b6daba1c685174789c3590c2bb3b0eec
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-05-03 15:56:18 +0500

    carousl fixes

--- src/components/question-card/EmbededImage.jsx
+++ src/components/question-card/EmbededImage.jsx
@@ -9,7 +9,7 @@ export const EmbededImage = ({ description, url }) => {
   // const closeDialogue = () => setImageDialogue(false);
 
   return (
-    <div className="mx-[22px] mt-[12px] rounded-[9.183px] border border-[#DEE6F7] pt-2 tablet:mx-[60px] tablet:mt-[23px] tablet:border-[2.755px]">
+    <div className="mx-[22px] mt-[12px] rounded-[9.183px] border border-[#DEE6F7] py-2 tablet:mx-[60px] tablet:mt-[23px] tablet:border-[2.755px]">
       {/* <FullScreenPicturePopup handleClose={closeDialogue} modalVisible={imageDialogue} content={url} /> */}
       {/* <h2 className="mb-1 text-[8px] font-medium text-[#7C7C7C] tablet:text-[14.692px]">{description}</h2> */}
       {url?.length === 1 ? (
