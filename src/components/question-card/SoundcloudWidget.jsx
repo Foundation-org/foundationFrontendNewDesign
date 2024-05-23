1967bcbd65a1743dde6f55e4dd24b369469052aa
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-05-23 16:47:06 +0500

    soundcloud new way of handle embed track

--- src/components/question-card/SoundcloudWidget.jsx
+++ src/components/question-card/SoundcloudWidget.jsx
@@ -1,29 +1,51 @@
-import React, { useState, useEffect, useRef } from 'react';
-import loadscript from 'load-script';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import { getQuestUtils, setIsShowPlayer, setPlayingPlayerId, toggleMedia } from '../../features/quest/utilsSlice';
-
-// SoundCloud widget API
-//  https://developers.soundcloud.com/docs/api/html5-widget
+import { useSelector } from 'react-redux';
 
 function SoundcloudWidget({ SCurl, playing, questId }) {
-  // state
+  const iframeRef = useRef(null);
   const dispatch = useDispatch();
   const questUtilsState = useSelector(getQuestUtils);
 
-  // populated once SoundCloud Widget API is loaded and initialized
-  const [player, setPlayer] = useState(null);
+  useEffect(() => {
+    const widget = SC.Widget(iframeRef.current);
 
-  // ref for iframe element - https://reactjs.org/docs/refs-and-the-dom.html
-  const iframeRef = useRef(null);
+    widget.bind(SC.Widget.Events.READY, () => {
+      console.log('SoundCloud Widget Ready');
+    });
 
-  // initialization - load soundcloud widget API and set SC event listeners
+    widget.bind(SC.Widget.Events.PLAY, () => {
+      dispatch(setPlayingPlayerId(questId));
+
+      if (!playing) {
+        dispatch(toggleMedia(true));
+      }
+      dispatch(setIsShowPlayer(true));
+    });
+
+    widget.bind(SC.Widget.Events.PAUSE, () => {
+      console.log('SoundCloud Widget Pause');
+      if (playing) {
+        dispatch(toggleMedia(false));
+      }
+    });
+
+    widget.bind(SC.Widget.Events.FINISH, () => {
+      console.log('SoundCloud Widget Finish');
+    });
+
+    widget.bind(SC.Widget.Events.ERROR, (error) => {
+      console.error('SoundCloud Widget Error', error);
+      handleVideoEnded();
+    });
+  }, []);
+
   const handleVideoEnded = () => {
-    console.log('running');
-    if (questUtilsState.loop) {
-      if (player) {
-        player.seekTo(0);
-        player.play();
+    if (questUtilsState.loop === true) {
+      if (playerRef.current) {
+        playerRef.current.seekTo(0);
+        playerRef.current.getInternalPlayer().play(); // Resume playback
       }
     } else {
       const index = questUtilsState.playingIds.findIndex((mediaId) => mediaId === questUtilsState.playerPlayingId);
@@ -38,61 +60,14 @@ function SoundcloudWidget({ SCurl, playing, questId }) {
       }
     }
   };
-  useEffect(() => {
-    // use load-script module to load SC Widget API
-    loadscript('https://w.soundcloud.com/player/api.js', () => {
-      // initialize player and store reference in state
-      const player = window.SC.Widget(iframeRef.current);
-      setPlayer(player);
-
-      const { PLAY, PAUSE, FINISH, ERROR } = window.SC.Widget.Events;
-
-      // NOTE: closures created - cannot access react state or props from within and SC callback functions!!
-      player.bind(PLAY, () => {
-        // update state to playing
-        dispatch(setPlayingPlayerId(questId));
-        dispatch(toggleMedia(true));
-        dispatch(setIsShowPlayer(true));
-      });
-
-      player.bind(PAUSE, () => {
-        // update state if player has paused - must double check isPaused since false positives
-        player.isPaused((playerIsPaused) => {
-          if (playerIsPaused) dispatch(toggleMedia(false));
-        });
-      });
-
-      player.bind(FINISH, () => {
-        handleVideoEnded();
-      });
-
-      player.bind(ERROR, (e) => {
-        console.error('SoundCloud Player Error:', e);
-      });
-    });
-  }, [dispatch, questId, handleVideoEnded]);
-
-  // integration - update SC player based on new state (e.g. play button in React section was clicked)
-  // adjust playback in SC player to match isPlaying state
-  useEffect(() => {
-    if (!player) return; // player loaded async - make sure available
-
-    player.isPaused((playerIsPaused) => {
-      if (!playing && playerIsPaused) {
-        player.play();
-      } else if (playing && !playerIsPaused) {
-        player.pause();
-      }
-    });
-  }, [playing, player]);
 
   return (
     <iframe
-      className="soundcloud-iframe"
       ref={iframeRef}
-      id="sound-cloud-player"
-      style={{ border: 'none', width: '100%' }}
+      className="soundcloud-iframe"
+      width="100%"
       scrolling="no"
+      frameBorder="no"
       allow="autoplay"
       src={`https://w.soundcloud.com/player/?url=${SCurl}&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false&show_playcount=false&sharing=false&buying=false&download=false&single_active=true`}
     ></iframe>
