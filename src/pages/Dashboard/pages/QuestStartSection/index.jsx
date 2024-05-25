a8f98994d9123f5b1258779fa812354a54d81ca4
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-05-25 14:43:56 +0500

    player should be closer to bottom

--- src/pages/Dashboard/pages/QuestStartSection/index.jsx
+++ src/pages/Dashboard/pages/QuestStartSection/index.jsx
@@ -160,7 +160,7 @@ const QuestStartSection = () => {
           </div>
         </div>
         {questUtils.isShowPlayer && (
-          <div className="absolute bottom-8 left-1/2 block -translate-x-1/2 laptop:hidden">
+          <div className="absolute bottom-0 left-1/2 block -translate-x-1/2 laptop:hidden">
             <div className="relative">
               <img
                 src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/mediaCloseIcon.svg`}
