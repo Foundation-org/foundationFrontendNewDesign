d2739cb51cc2f72d1f110ecc6b67ced11075fe71
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-05-27 11:50:55 +0500

    Fixes

--- src/components/posts/SystemNotificationCard.jsx
+++ src/components/posts/SystemNotificationCard.jsx
@@ -5,9 +5,9 @@ const SystemNotificationCard = ({ post }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col gap-2 rounded-[13.842px] border-[1.846px] border-[#D9D9D9] bg-[#F4F8FF] px-7 py-[14px] tablet:gap-4 tablet:px-[44px] tablet:py-6">
+    <div className="flex flex-col gap-2 rounded-[13.842px] border-[1.846px] border-[#D9D9D9] bg-[#F4F8FF] px-7 pb-[15px] pt-[14px] tablet:gap-4 tablet:px-[44px] tablet:py-6">
       <h1 className="text-[13px] font-bold leading-normal text-[#5B5B5B] tablet:text-[22px]">{post.header}</h1>
-      <p className="text-[12px] font-normal leading-normal text-[#7C7C7C] tablet:text-[18px] tablet:leading-[25px]">
+      <p className="text-[13px] font-normal leading-normal text-[#7C7C7C] tablet:text-[18px] tablet:leading-[25px]">
         {post.text}
       </p>
       <div className="flex justify-end">
