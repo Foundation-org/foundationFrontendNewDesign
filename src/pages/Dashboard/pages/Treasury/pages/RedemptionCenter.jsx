b7b66d636cf2458ee4c54b12f385af9efdb62178
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-06-12 17:27:13 +0500

    design for tabs and greater

--- src/pages/Dashboard/pages/Treasury/pages/RedemptionCenter.jsx
+++ src/pages/Dashboard/pages/Treasury/pages/RedemptionCenter.jsx
@@ -366,14 +366,23 @@ export default function RedemptionCenter() {
           <div className="flex flex-col gap-[5px] rounded-b-[10px] border-[#D9D9D9] bg-[#FDFDFD] px-5 py-[10px] tablet:gap-[15px] tablet:border-[1.85px] tablet:px-9 tablet:py-[18.73px]">
             <div className="rounded-[7.546px] border-[2.792px] border-[#D9D9D9]">
               <div className="grid grid-cols-12 border-b-[2.792px] border-[#D9D9D9]">
-                <div className="col-span-3 px-[15px] py-3">
+                <div className="col-span-3 px-3 py-2">
                   <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Code</h1>
                 </div>
-                <div className="col-span-5 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-3 py-2">
                   <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Description</h1>
                 </div>
-                <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-3 py-2">
                   <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">FDX cost</h1>
+                </div>
+                <div className="col-span-1 border-l-[2.792px] border-[#D9D9D9] px-1 py-2">
+                  <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Share</h1>
+                </div>
+                <div className="col-span-1 border-l-[2.792px] border-[#D9D9D9] px-1 py-2">
+                  <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Copy</h1>
+                </div>
+                <div className="col-span-3 border-l-[2.792px] border-[#D9D9D9] px-3 py-2">
+                  <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]"></h1>
                 </div>
                 {/* <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9]  px-[15px] py-3">
                   <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Expiration date</h1>
@@ -387,15 +396,45 @@ export default function RedemptionCenter() {
                 </div>
               ) : (
                 unredeemedData?.data?.data?.map((item, index) => (
-                  <div className="grid grid-cols-12" key={index + 1}>
-                    <div className="col-span-3 px-[15px] py-3">
+                  <div
+                    className={`grid grid-cols-12 border-b-[2.792px] border-[#D9D9D9] ${unredeemedData?.data?.data.length > 1 && unredeemedData?.data?.data.length - 1 === index ? 'border-b-0' : ''}`}
+                    key={index + 1}
+                  >
+                    <div className="col-span-3 px-3 py-2">
                       <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">{item.code}</h1>
                     </div>
-                    <div className="col-span-5 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                    <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-3 py-2">
                       <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">{item.description}</h1>
                     </div>
-                    <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                    <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-3 py-2">
                       <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">{item.amount} FDX</h1>
+                    </div>
+                    <div className="col-span-1 border-l-[2.792px] border-[#D9D9D9] px-3 py-2">
+                      <img
+                        src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/sharelink.svg`}
+                        alt="copy"
+                        className="h-3 w-3 cursor-pointer tablet:h-[23px] tablet:w-[23px]"
+                        onClick={() => {
+                          handleShareLink(item.code);
+                          toast.success('Share Link copied!');
+                        }}
+                      />
+                    </div>
+                    <div className="col-span-1 border-l-[2.792px] border-[#D9D9D9] px-3 py-2">
+                      <img
+                        src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/copy.svg`}
+                        alt="copy"
+                        className="h-3 w-3 cursor-pointer tablet:h-[23px] tablet:w-[23px]"
+                        onClick={() => {
+                          copyToClipboard(item.code);
+                          toast.success('Code Copied!');
+                        }}
+                      />
+                    </div>
+                    <div className="col-span-3 flex w-full justify-center border-l-[2.792px] border-[#D9D9D9] px-3 py-2">
+                      <Button variant="result" className={'max-w-[124px]'} onClick={() => handleRedeeem(item.code)}>
+                        {radeemLoading === item.code ? <FaSpinner className="animate-spin text-[#EAEAEA]" /> : 'Redeem'}
+                      </Button>
                     </div>
                     {/* <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
                       <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">
@@ -403,7 +442,7 @@ export default function RedemptionCenter() {
                         Never
                       </h1>
                     </div> */}
-                    <div className="col-span-12 flex w-full justify-center gap-6 border-y-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                    {/* <div className="col-span-12 flex w-full justify-center gap-6 border-y-[2.792px] border-[#D9D9D9] px-[15px] py-3">
                       <Button
                         variant="share-link"
                         className={'max-w-[141px] tablet:h-[50px]'}
@@ -427,7 +466,7 @@ export default function RedemptionCenter() {
                       <Button variant="result" className={'max-w-[124px]'} onClick={() => handleRedeeem(item.code)}>
                         {radeemLoading === item.code ? <FaSpinner className="animate-spin text-[#EAEAEA]" /> : 'Redeem'}
                       </Button>
-                    </div>
+                    </div> */}
                   </div>
                 ))
               )}
@@ -459,11 +498,14 @@ export default function RedemptionCenter() {
                 <div className="col-span-3 px-[15px] py-3">
                   <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Code</h1>
                 </div>
-                <div className="col-span-5 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                <div className="col-span-4 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
                   <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Description</h1>
                 </div>
                 <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
                   <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">FDX cost</h1>
+                </div>
+                <div className="col-span-3 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                  <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Redeemed date</h1>
                 </div>
                 {/* <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9]  px-[15px] py-3">
                   <h1 className="text-[12px] font-semibold text-[#85898C] tablet:text-[16px]">Expiration date</h1>
@@ -480,13 +522,13 @@ export default function RedemptionCenter() {
                   ?.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
                   .map((item, index) => (
                     <div
-                      className={`grid grid-cols-12 ${index === 0 && isPulse ? 'text-[#049952]' : 'text-[#A3A3A3]'}`}
+                      className={`grid grid-cols-12 border-b border-[#D9D9D9] ${index === 0 && isPulse ? 'text-[#049952]' : 'text-[#A3A3A3]'} ${history?.data?.data.length > 1 && history?.data?.data.length - 1 === index ? 'border-b-0' : ''}`}
                       key={index + 1}
                     >
                       <div className="col-span-3 px-[15px] py-3">
                         <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">{item.code}</h1>
                       </div>
-                      <div className="col-span-5 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                      <div className="col-span-4 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
                         <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">
                           {item.description}
                         </h1>
@@ -494,17 +536,22 @@ export default function RedemptionCenter() {
                       <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
                         <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">{item.amount} FDX</h1>
                       </div>
+                      <div className="col-span-3 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                        <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">
+                          {formatDate(item.createdAt)}
+                        </h1>
+                      </div>
                       {/* <div className="col-span-2 border-l-[2.792px] border-[#D9D9D9] px-[15px] py-3">
                         <h1 className="text-[12px] font-normal text-[#85898C] tablet:text-[16px]">
                           {calculateExpiry(item.expiry)}
                           Never
                         </h1>
                       </div> */}
-                      <div className="col-span-12 flex w-full justify-center gap-6 border-y-[2.792px] border-[#D9D9D9] px-[15px] py-3">
+                      {/* <div className="col-span-12 flex w-full justify-center gap-6 border-y-[2.792px] border-[#D9D9D9] px-[15px] py-3">
                         <p className="text-[9px] font-medium leading-normal tablet:text-[20px]">
                           {formatDate(item.createdAt)}
                         </p>
-                        {/* <img
+                        <img
                           src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/dashboard/trash2.svg`}
                           alt="trash"
                           className="h-3 w-[9px] cursor-pointer tablet:h-[23px] tablet:w-[17.6px]"
@@ -512,8 +559,8 @@ export default function RedemptionCenter() {
                             setDeleteHistoryCode(item.code);
                             setIsDeleteModal(true);
                           }}
-                        /> */}
-                      </div>
+                        />
+                      </div> */}
                     </div>
                   ))
               )}
