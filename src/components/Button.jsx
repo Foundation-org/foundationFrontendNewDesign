532197009b83180cebd8071cf8162909dc988cc6
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-06-13 22:08:17 +0500

    Fixes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -39,7 +39,7 @@ const Button = ({
 
   return (
     <button type={type} className={buttonClasses} disabled={disabled} onClick={onClick}>
-      <div className="flex min-w-[115px] items-center">{children}</div>
+      <div className="flex min-w-[75px] items-center tablet:min-w-[115px]">{children}</div>
     </button>
   );
 };
