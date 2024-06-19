007d4a6c339bb70eee6aacf5e7feef52a07f195d
Haider Yaqoob <haideryaqoob720@gmail.com>
2024-06-19 20:48:55 +0500

    guest mode 2 pages

--- src/pages/Dashboard/pages/CustomerSupport/GuestCustomerSupport.jsx
+++ src/pages/Dashboard/pages/CustomerSupport/GuestCustomerSupport.jsx
@@ -21,6 +21,8 @@ const GuestCustomerSupport = () => {
   const list = [
     { id: 1, title: 'About', path: '/help/about' },
     { id: 2, title: "FAQ's", path: '/help/faq' },
+    { id: 3, title: 'Terms of Service', path: '/dashboard/help/terms-of-service' },
+    { id: 4, title: 'Privacy Policy', path: '/dashboard/help/privacy-policy' },
     { id: 3, title: 'Contact Us', path: '/help/contact-us' },
   ];
 
