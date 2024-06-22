e7285a2bf1a2dfde80776501d51876a13678f5b6
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-06-22 17:44:14 +0500

    new test added

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import showToast from './ui/Toast';
+import { useState } from 'react';
+import { Reorder } from 'framer-motion';
+
+const initialItems = ['🍅 Tomato', '🥒 Cucumber', '🧀 Cheese', '🥬 Lettuce'];
 
 const Test = () => {
-  const e = 'hamza is a boy';
+  const [items, setItems] = useState(initialItems);
+
   return (
-    <div>
-      Test
-      <button onClick={() => showToast('error', 'error', {}, e)}>Click Me</button>
-      <button onClick={() => showToast('info', 'verificationEmailSent')}>Click Me</button>
-      <button onClick={() => showToast('success', 'verificationEmailSent')}>Click Me</button>
-      <button onClick={() => showToast('warning', 'verificationEmailSent')}>Click Me</button>
-      <button onClick={() => showToast('error', 'verificationEmailSent')}>PredefinedError</button>
+    <div className="m-0 flex h-full w-full max-w-[778px] items-center justify-center overflow-hidden bg-[#ffaa00] p-0">
+      <Reorder.Group axis="y" onReorder={setItems} values={items} className="relative w-[300px]">
+        {items.map((item) => (
+          <Reorder.Item
+            key={item}
+            value={item}
+            id={item}
+            className="mb-[10px] flex w-full flex-shrink-0 cursor-grab items-center justify-between rounded-[10px] bg-white px-[18px] py-[15px]"
+          >
+            <span>{item}</span>
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
     </div>
   );
 };
