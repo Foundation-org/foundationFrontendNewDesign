18fe50179db61c1ebd507b0c186aaed705a3bb15
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-06-27 18:03:15 +0500

    test updated

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -7,7 +7,7 @@ const Test = () => {
   const [items, setItems] = useState(initialItems);
 
   return (
-    <div className="m-0 flex h-full w-full max-w-[778px] items-center justify-center overflow-hidden bg-[#ffaa00] p-0">
+    <div className="m-0 flex h-full w-full items-center justify-center overflow-hidden bg-[#ffaa00] p-0">
       <Reorder.Group axis="y" onReorder={setItems} values={items} className="relative w-[300px]">
         {items.map((item) => (
           <Reorder.Item
