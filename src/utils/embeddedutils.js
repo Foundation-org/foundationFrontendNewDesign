6209d9711799c5fbf28b345936b6b9564201ce4c
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-06-27 17:22:24 +0500

    Added short reels check in youtube embed

--- src/utils/embeddedutils.js
+++ src/utils/embeddedutils.js
@@ -1,14 +1,18 @@
 export function extractYouTubeVideoId(url) {
   const patternLong = /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/;
   const patternShort = /youtu\.be\/([^"&?/ ]{11})/;
+  const patternShorts = /youtube\.com\/shorts\/([^"&?/ ]{11})/;
 
   const matchLong = url.match(patternLong);
   const matchShort = url.match(patternShort);
+  const matchShorts = url.match(patternShorts);
 
   if (matchLong && matchLong[1]) {
     return matchLong[1]; // Full YouTube URL
   } else if (matchShort && matchShort[1]) {
     return matchShort[1]; // Short YouTube URL
+  } else if (matchShorts && matchShorts[1]) {
+    return matchShorts[1]; // YouTube Shorts URL
   } else {
     return null; // Invalid or unsupported URL
   }
