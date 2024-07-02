5eac741dcc7c79e8c3351ce38b51304bcddedddd
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-07-02 21:25:52 +0500

    reverted blue badge

--- src/components/question-card/options/SingleAnswerRankedChoice.jsx
+++ src/components/question-card/options/SingleAnswerRankedChoice.jsx
@@ -229,17 +229,17 @@ const SingleAnswerRankedChoice = (props) => {
           <div className="flex w-7 min-w-[28px] items-center justify-center bg-transparent tablet:h-[33px] tablet:w-[26.48px] dark:bg-[#000]">
             <img
               src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/addOptions/yellowBadge.svg`}
-              alt="trash"
+              alt="yellow badge"
               className="h-[15.5px] w-[12.44px] tablet:h-[27px] tablet:w-[21px]"
             />
           </div>
         ) : (
           <div className="flex w-7 min-w-[28px] items-center justify-center bg-transparent tablet:h-[33px] tablet:w-[26.48px] dark:bg-[#000]">
-            {/* <img
+            <img
               src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/addOptions/blueBadge.svg`}
               alt="blue badge"
               className="h-[15.5px] w-[12.44px] tablet:h-[27px] tablet:w-[21px]"
-            /> */}
+            />
           </div>
         )
       ) : (
