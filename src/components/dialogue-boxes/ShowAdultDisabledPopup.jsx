28c9e41b05f873144a13170f233eccb3184e4d58
Wamiq <wamiqakram@gmail.com>
2024-07-08 22:39:38 +0500

    period issue

--- src/components/dialogue-boxes/ShowAdultDisabledPopup.jsx
+++ src/components/dialogue-boxes/ShowAdultDisabledPopup.jsx
@@ -7,7 +7,7 @@ export default function ShowAdultDisabledPopup({ handleClose, modalVisible, titl
       <div className="px-[18px] py-[10px] tablet:px-[55px] tablet:py-[25px]">
         <h1 className="text-[10px] font-medium leading-[12px] text-[#707175] tablet:text-[20px] tablet:leading-[24.2px]">
           This post has been marked as adult content. You do not currently have adult posts enabled in your filter
-          settings. You will not be able to see this post in your feed
+          settings. You will not be able to see this post in your feed.
         </h1>
         <div className="mt-[10px] flex justify-end gap-[15px] tablet:mt-[25px] tablet:gap-[34px]">
           <Button variant={'submit'} onClick={handleClose}>
