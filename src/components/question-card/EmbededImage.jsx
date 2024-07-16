d7875c15570d43c753a7b82f1d648c04ee029698
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-07-16 21:54:35 +0500

    image size increased

--- src/components/question-card/EmbededImage.jsx
+++ src/components/question-card/EmbededImage.jsx
@@ -10,13 +10,13 @@ export const EmbededImage = ({ description, url, id }) => {
   const closeDialogue = () => setImageDialogue(false);
 
   return (
-    <div className="mx-[22px] mt-[12px] rounded-[9.183px] border border-[#DEE6F7] py-2 tablet:mx-[60px] tablet:mt-[23px] tablet:border-[2.755px]">
+    <div className="mx-[22px] mt-[12px] rounded-[9.183px] border border-[#DEE6F7] px-4 py-2 tablet:mx-[60px] tablet:mt-[23px] tablet:border-[2.755px]">
       <FullScreenPictureViewer handleClose={closeDialogue} modalVisible={imageDialogue} content={url} />
       {/* <h2 className="mb-1 text-[8px] font-medium text-[#7C7C7C] tablet:text-[14.692px]">{description}</h2> */}
       {url?.length === 1 ? (
         <img
           src={url[0]}
-          className="h-[138px] w-full rounded-[4.098px] object-contain tablet:h-[372px] tablet:rounded-[15px]"
+          className="h-full max-h-[230px] w-full rounded-[4.098px] object-contain tablet:h-[372px] tablet:rounded-[15px]"
           onClick={openDialogue}
         />
       ) : (
