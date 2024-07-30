1da8cd6db1db373a82f431a73e949b25cf2b3e45
Hamzazaheer1 <mhamzazaheer1@gmail.com>
2024-07-30 16:00:13 +0500

    changes

--- src/pages/Dashboard/pages/Quest/components/AddMedia.jsx
+++ src/pages/Dashboard/pages/Quest/components/AddMedia.jsx
@@ -122,7 +122,7 @@ export default function AddMedia({ handleTab }) {
   return (
     <div>
       {getMediaStates?.isMedia?.isMedia ? (
-        <div className="w-[calc(100%-51.75px] relative mx-[15px] mt-3 flex flex-col gap-[6px] rounded-[7.175px] border border-white-500 p-[15px] px-[5px] py-[10px] tablet:mx-11 tablet:mt-[25px] tablet:gap-[15px] tablet:border-[2.153px] tablet:px-[15px] tablet:py-[25px] dark:border-gray-250">
+        <div className="w-[calc(100%-51.75px] relative mt-3 flex flex-col gap-[6px] rounded-[7.175px] border border-white-500 p-[15px] px-[5px] py-[10px] tablet:mt-[25px] tablet:gap-[15px] tablet:border-[2.153px] tablet:px-[15px] tablet:py-[25px] dark:border-gray-250">
           <h1 className="absolute -top-[5.5px] left-5 bg-white text-[10px] font-semibold leading-[10px] text-[#707175] tablet:-top-[11px] tablet:left-9 tablet:text-[20px] tablet:leading-[20px] dark:bg-gray-200 dark:text-white-400">
             {getMediaStates.isMedia.type === 'EmbedVideo' ? 'Video' : 'Audio'}
           </h1>
