0fa981d00bc55532c2b10efee4ebd65889c15dc4
Wamiq <wamiqakram@gmail.com>
2024-08-22 19:09:06 +0500

    badges length corrected

--- src/constants/varification-badges.js
+++ src/constants/varification-badges.js
@@ -342,4 +342,4 @@ export const subscription = [
   },
 ];
 
-export const badgesTotalLength = contacts.length + socials.length + web3.length + legacy.length + personal.length - 2;
+export const badgesTotalLength = contacts.length + socials.length + web3.length + legacy.length + personal.length - 1;
