72c387598335fe10cccacb621e5999b2defc98d7
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-09-03 16:56:30 +0500

    1.15.1

--- src/components/question-card/Spacing.jsx
+++ src/components/question-card/Spacing.jsx
@@ -4,6 +4,35 @@ const Spacing = ({ questStartData, show, postProperties }) => {
   const renderQuestInfoText = () => {
     if (show) {
       return (
+        // <div className="relative">
+        //   {(questStartData?.startQuestData?.isFeedback &&
+        //     questStartData.startStatus !== 'continue' &&
+        //     questStartData.startStatus !== 'change answer') ||
+        //   questStartData.isClosed ? (
+        //     <div className="flex h-[23px] items-end justify-center tablet:h-[50px]">
+        //       <h4 className="text-center text-[10px] font-semibold leading-[10px] text-red-500 dark:text-accent-300 tablet:py-[10px] tablet:text-[1rem] tablet:leading-[1rem]">
+        //         Participation is closed
+        //       </h4>{' '}
+        //     </div>
+        //   ) : questStartData.whichTypeQuestion === 'ranked choise' ? (
+        //     <h4 className="max-h-[24.16px] min-h-[24.16px] text-center text-[7.5px] font-normal text-[#85898C] tablet:max-h-[50px] tablet:min-h-[50px] tablet:text-[1rem]">
+        //       &#x200B;
+        //     </h4>
+        //   ) : (questStartData.whichTypeQuestion === 'multiple choise' ||
+        //       questStartData.whichTypeQuestion === 'open choice') &&
+        //     questStartData.userCanSelectMultiple ? (
+        //     <h4 className="max-h-[24.16px] min-h-[24.16px] text-center text-[7.5px] font-normal text-[#85898C] tablet:max-h-[50px] tablet:min-h-[50px] tablet:text-[1rem]">
+        //       &#x200B;
+        //     </h4>
+        //   ) : (
+        //     <h4 className="max-h-[24.16px] min-h-[24.16px] text-center text-[7.5px] font-normal text-[#85898C] tablet:max-h-[50px] tablet:min-h-[50px] tablet:text-[1rem]">
+        //       &#x200B;
+        //     </h4>
+        //   )}
+        //   {!questStartData.isClosed && questStartData.type === 'embed' && (
+        //     <EmbedParticipate postProperties={postProperties} />
+        //   )}
+        // </div>
         <div className="relative">
           {(questStartData?.startQuestData?.isFeedback &&
             questStartData.startStatus !== 'continue' &&
@@ -12,23 +41,16 @@ const Spacing = ({ questStartData, show, postProperties }) => {
             <div className="flex h-[23px] items-end justify-center tablet:h-[50px]">
               <h4 className="text-center text-[10px] font-semibold leading-[10px] text-red-500 dark:text-accent-300 tablet:py-[10px] tablet:text-[1rem] tablet:leading-[1rem]">
                 Participation is closed
-              </h4>{' '}
+              </h4>
             </div>
-          ) : questStartData.whichTypeQuestion === 'ranked choise' ? (
-            <h4 className="max-h-[24.16px] min-h-[24.16px] text-center text-[7.5px] font-normal text-[#85898C] tablet:max-h-[50px] tablet:min-h-[50px] tablet:text-[1rem]">
-              &#x200B;
-            </h4>
-          ) : (questStartData.whichTypeQuestion === 'multiple choise' ||
-              questStartData.whichTypeQuestion === 'open choice') &&
-            questStartData.userCanSelectMultiple ? (
-            <h4 className="max-h-[24.16px] min-h-[24.16px] text-center text-[7.5px] font-normal text-[#85898C] tablet:max-h-[50px] tablet:min-h-[50px] tablet:text-[1rem]">
-              &#x200B;
-            </h4>
           ) : (
-            <h4 className="max-h-[24.16px] min-h-[24.16px] text-center text-[7.5px] font-normal text-[#85898C] tablet:max-h-[50px] tablet:min-h-[50px] tablet:text-[1rem]">
+            <h4
+              className={`${questStartData.type === 'embed' ? 'max-h-[35px] min-h-[35px] tablet:max-h-[65px] tablet:min-h-[65px]' : 'max-h-[24.16px] min-h-[24.16px] tablet:max-h-[50px] tablet:min-h-[50px]'} text-center text-[7.5px] font-normal text-[#85898C] tablet:text-[1rem]`}
+            >
               &#x200B;
             </h4>
           )}
+
           {!questStartData.isClosed && questStartData.type === 'embed' && (
             <EmbedParticipate postProperties={postProperties} />
           )}
