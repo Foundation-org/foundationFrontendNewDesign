111e478c9c0fdbfbcbedc36e366e765f9ea549a1
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-09-04 15:09:23 +0500

    refactoring questStartSection + new hooks for start change and guest submit

--- src/pages/Dashboard/pages/QuestStartSection/index.jsx
+++ src/pages/Dashboard/pages/QuestStartSection/index.jsx
@@ -1,138 +1,70 @@
-import { useEffect } from 'react';
-import * as filtersActions from '../../../../features/sidebar/filtersSlice';
-import * as questUtilsActions from '../../../../features/quest/utilsSlice';
-import MediaControls from '../../../../components/MediaControls';
-import SidebarLeft from '../../components/SidebarLeft';
-import QuestionCardWithToggle from './components/QuestionCardWithToggle';
-import Slider from '../../../../components/Slider';
-import api from '../../../../services/api/Axios';
+import { useEffect, useMemo } from 'react';
 import { printEndMessage } from '../../../../utils';
 import { useSelector, useDispatch } from 'react-redux';
 import { useInView } from 'react-intersection-observer';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useFetchPosts } from '../../../../services/queries/post';
+import { getFilters } from '../../../../features/sidebar/filtersSlice';
+import * as questUtilsActions from '../../../../features/quest/utilsSlice';
+import Slider from '../../../../components/Slider';
+import SidebarLeft from '../../components/SidebarLeft';
+import MediaControls from '../../../../components/MediaControls';
+import QuestionCardWithToggle from './components/QuestionCardWithToggle';
 import SystemNotificationCard from '../../../../components/posts/SystemNotificationCard';
-import ShowAdultDisabledPopup from '../../../../components/dialogue-boxes/ShowAdultDisabledPopup';
 
 const QuestStartSection = () => {
   const dispatch = useDispatch();
-  const { ref, inView } = useInView();
   const persistedUserInfo = useSelector((state) => state.auth.user);
-  const filterStates = useSelector(filtersActions.getFilters);
+  const filterStates = useSelector(getFilters);
   const questUtils = useSelector(questUtilsActions.getQuestUtils);
 
-  const fetchPosts = async function getInfoQuestions({ pageParam, signal }) {
-    const params = {
-      _page: pageParam,
-      _limit: 5,
-      start: (pageParam - 1) * 5,
-      end: pageParam * 5,
-      uuid: persistedUserInfo.uuid,
-      sort: filterStates.filterBySort === '' ? 'Newest First' : filterStates.filterBySort,
-      type: filterStates.filterByType,
-      filter: filterStates.filterByScope === 'Me' && true,
-      participated:
-        filterStates.filterByStatus === 'Participated'
-          ? 'Yes'
-          : filterStates.filterByStatus === 'Not Participated'
-            ? 'Not'
-            : 'All',
-      moderationRatingInitial: filterStates.moderationRatingFilter.initial,
-      moderationRatingFinal: filterStates.moderationRatingFilter.final,
-      terms: filterStates.searchData,
-      Page: filterStates.bookmarks ? 'Bookmark' : '',
-      media: filterStates.filterByMedia,
-    };
+  const { ref, inView } = useInView();
 
-    if (filterStates.topics.Block.list.length !== 0) {
-      params.blockedTerms = JSON.stringify(filterStates.topics.Block.list);
-    }
-
-    const response = await api.get('/infoquestions/getQuestsAll', { params, signal });
-
-    return response.data.data;
-  };
-
-  const { data, status, error, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
-    queryKey: [
-      'posts',
-      filterStates.filterBySort,
-      filterStates.filterByType,
-      filterStates.filterByScope,
-      filterStates.moderationRatingFilter.initial,
-      filterStates.moderationRatingFilter.final,
-      filterStates.searchData,
-      filterStates.filterByStatus,
-      filterStates.topics.Block.list,
-      filterStates.bookmarks,
-      filterStates.filterByMedia,
-    ],
-    queryFn: ({ pageParam, signal }) => fetchPosts({ pageParam, signal }),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages) => {
-      const nexPage = lastPage.length ? allPages.length + 1 : undefined;
-      return nexPage;
-    },
-  });
+  const { data, status, error, fetchNextPage, hasNextPage, isFetching } = useFetchPosts(
+    filterStates,
+    persistedUserInfo,
+  );
 
   useEffect(() => {
     if (inView && hasNextPage) {
       fetchNextPage();
     }
     dispatch(questUtilsActions.setNextPage(hasNextPage));
-  }, [inView, hasNextPage, fetchNextPage]);
+  }, [inView, hasNextPage, fetchNextPage, dispatch]);
 
   if (status === 'error') {
     return <p>Error: {error.message}</p>;
   }
 
-  const content = data?.pages.map((posts) =>
-    posts.map((post, index) => {
-      if (post.id === 'system_notification' && posts.length == index + 1) {
-        return <SystemNotificationCard key={index + 1} post={post} innerRef={ref} />;
-      } else if (post.id === 'system_notification' && posts.length !== index + 1) {
-        return <SystemNotificationCard key={index + 1} post={post} />;
-      } else {
-        if (posts.length == index + 1) {
+  const content = useMemo(
+    () =>
+      data?.pages.map((posts) =>
+        posts.map((post, index) => {
+          const isLastPost = posts.length === index + 1;
+
+          if (post.id === 'system_notification') {
+            return <SystemNotificationCard key={index + 1} post={post} innerRef={isLastPost ? ref : null} />;
+          }
+
           return (
             <QuestionCardWithToggle
-              innerRef={ref}
               key={post._id}
+              innerRef={isLastPost ? ref : null}
               questStartData={post}
               playing={post._id === questUtils.playerPlayingId && questUtils.isMediaPlaying}
             />
           );
-        } else {
-          return (
-            <QuestionCardWithToggle
-              key={post._id}
-              questStartData={post}
-              playing={post._id === questUtils.playerPlayingId && questUtils.isMediaPlaying}
-            />
-          );
-        }
-      }
-    }),
+        }),
+      ),
+    [data, ref, questUtils],
   );
 
   return (
-    <div className="mx-auto w-full max-w-[1440px] bg-[#F2F3F5] laptop:mx-[331px] desktop:mx-auto dark:bg-black">
-      {/* {filterStates?.moderationRatingFilter?.initial === 0 &&
-        filterStates?.moderationRatingFilter?.final === 0 &&
-        questUtils.adultFilterPopup.rating !== 0 && (
-          <ShowAdultDisabledPopup
-            handleClose={() => {
-              dispatch(questUtilsActions.addAdultFilterPopup({ rating: 0 }));
-            }}
-            modalVisible={questUtils.adultFilterPopup.rating !== 0}
-            title={'Adult Post Warning'}
-            image={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/hiddenposts/unhide/adult_icon.svg`}
-          />
-        )} */}
+    <div className="mx-auto w-full max-w-[1440px] bg-[#F2F3F5] dark:bg-black laptop:mx-[331px] desktop:mx-auto">
       <div className="relative mx-auto flex w-full max-w-[778px] flex-col laptop:flex-row">
         <div className="block tablet:hidden">
           <SidebarLeft />
         </div>
-        <div className="mx-auto flex h-full max-h-[calc(100dvh-134px)] min-h-[calc(100dvh-134px)] w-full max-w-[778px] flex-col overflow-y-hidden bg-[#F2F3F5] no-scrollbar tablet:max-h-[calc(100dvh-172px)] tablet:min-h-[calc(100dvh-172px)] laptop:max-h-[calc(100dvh-70px)] laptop:min-h-[calc(100dvh-70px)] dark:bg-black">
+        <div className="mx-auto flex h-full max-h-[calc(100dvh-134px)] min-h-[calc(100dvh-134px)] w-full max-w-[778px] flex-col overflow-y-hidden bg-[#F2F3F5] no-scrollbar dark:bg-black tablet:max-h-[calc(100dvh-172px)] tablet:min-h-[calc(100dvh-172px)] laptop:max-h-[calc(100dvh-70px)] laptop:min-h-[calc(100dvh-70px)]">
           <div className="fixed left-auto right-auto max-w-full laptop:max-w-[calc(100%-662px)] desktop:max-w-[calc(1440px-662px)]">
             <Slider isFetching={isFetching} />
           </div>
---
+++ src/services/mutations/post.js
@@ -0,0 +1,163 @@
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { createStartQuest } from '../api/questsApi';
+import { resetaddOptionLimit } from '../../features/quest/utilsSlice';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { submitListResponse, updateCategoryParticipentsCount } from '../api/listsApi';
+import showToast from '../../components/ui/Toast';
+
+export const useStartGuestListPost = (setLoading) => {
+  const location = useLocation();
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: startGuestListPost } = useMutation({
+    mutationFn: submitListResponse,
+    onSuccess: (resp) => {
+      if (resp.status === 200) {
+        queryClient.invalidateQueries(['userInfo']);
+        queryClient.setQueriesData(['postsByCategory'], (oldData) => {
+          if (!oldData || !oldData.post) {
+            return oldData;
+          }
+
+          return {
+            ...oldData,
+            post: oldData.post.map((item) =>
+              item._id === resp.data.category.post._id ? resp.data.category.post : item,
+            ),
+          };
+        });
+
+        setLoading(false);
+
+        if (location.pathname.startsWith('/l/')) {
+          updateCategoryParticipentsCount({ categoryLink: location.pathname.split('/')[2] });
+        }
+      }
+    },
+    onError: (err) => {
+      console.log({ err });
+      showToast('error', 'error', {}, err.response.data.message.split(':')[1]);
+      setLoading(false);
+    },
+  });
+
+  return { startGuestListPost };
+};
+
+export const useStartPost = (setLoading, setSubmitResponse, handleViewResults, questStartData) => {
+  const location = useLocation();
+  const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+
+  const { mutateAsync: startPost } = useMutation({
+    mutationFn: createStartQuest,
+    onSuccess: (resp) => {
+      queryClient.invalidateQueries({ queryKey: ['userInfo', localStorage.getItem('uuid')] }, { exact: true });
+      queryClient.invalidateQueries({ queryKey: ['postsByCategory'] }, { exact: true });
+
+      queryClient.setQueriesData(['posts'], (oldData) => ({
+        ...oldData,
+        pages: oldData?.pages?.map((page) =>
+          page.map((item) => (item._id === resp.data.data._id ? resp.data.data : item)),
+        ),
+      }));
+
+      if (resp.data.message === 'Start Quest Created Successfully') {
+        setLoading(false);
+
+        queryClient.setQueryData(['questByShareLink'], (oldData) => {
+          if (!oldData) return resp.data.data;
+
+          return {
+            ...oldData,
+            data: {
+              ...oldData.data,
+              ...resp.data.data,
+              data: oldData.data.data.map((item) => (item._id === resp.data.data._id ? resp.data.data : item)),
+            },
+          };
+        });
+      }
+
+      if (location.pathname.startsWith('/post/')) {
+        setSubmitResponse(resp.data.data);
+      }
+      if (!location.pathname.startsWith('/p/' || !location.pathname.startsWith('/l'))) {
+        handleViewResults(questStartData._id);
+      }
+
+      if (location.pathname.startsWith('/l/')) {
+        updateCategoryParticipentsCount({ categoryLink: location.pathname.split('/')[2] });
+      }
+      dispatch(resetaddOptionLimit());
+    },
+    onError: (err) => {
+      console.log(err);
+      showToast('error', 'error', {}, err.response.data.message.split(':')[1]);
+      if (err.response.data.message === 'Sorry, this post has been deleted by the user who created it.') {
+        queryClient.setQueriesData(['posts'], (oldData) => ({
+          ...oldData,
+          pages: oldData?.pages?.map((page) => page.filter((item) => item._id !== err.response.data._id)),
+        }));
+        queryClient.setQueriesData({ queryKey: ['questByShareLink'] }, (oldData) => {
+          return {
+            ...oldData,
+            data: {
+              ...oldData.data,
+              data: oldData.data.data.filter((item) => item._id !== err.response.data._id),
+            },
+          };
+        });
+      }
+      setLoading(false);
+      dispatch(resetaddOptionLimit());
+    },
+  });
+
+  return { startPost };
+};
+
+export const useChangePost = (setLoading, setSubmitResponse, handleViewResults, questStartData) => {
+  const location = useLocation();
+  const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+
+  const { mutateAsync: changePost } = useMutation({
+    mutationFn: questServices.updateChangeAnsStartQuest,
+    onSuccess: (resp) => {
+      if (resp.data.message === 'Answer has not changed') {
+        setLoading(false);
+        showToast('warning', 'selectedSameOptions');
+      }
+      if (resp.data.message === 'You can change your answer once every 1 hour') {
+        showToast('warning', 'changeOptionTimePeriod');
+        setLoading(false);
+      }
+      if (resp.data.message === 'Start Quest Updated Successfully') {
+        queryClient.invalidateQueries({ queryKey: ['userInfo', localStorage.getItem('uuid')] }, { exact: true });
+        setLoading(false);
+        handleViewResults(questStartData._id);
+
+        if (location.pathname.startsWith('/post/')) {
+          setSubmitResponse(resp.data.data);
+        }
+
+        queryClient.setQueriesData(['posts'], (oldData) => ({
+          ...oldData,
+          pages: oldData?.pages?.map((page) =>
+            page.map((item) => (item._id === resp.data.data._id ? resp.data.data : item)),
+          ),
+        }));
+      }
+      dispatch(resetaddOptionLimit());
+    },
+    onError: (err) => {
+      showToast('error', 'error', {}, err.response.data.message.split(':')[1]);
+      setLoading(false);
+      dispatch(resetaddOptionLimit());
+    },
+  });
+
+  return { changePost };
+};
