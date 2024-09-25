454e6daaf1359b77c4859d8d3eacb885022908bd
Hamzazaheer1 <mhamzazaheer1@gmail.com>
2024-09-26 02:26:26 +0500

    fixed font size in news search + search icon in feedback given

--- src/pages/Dashboard/components/DashboardLayout.jsx
+++ src/pages/Dashboard/components/DashboardLayout.jsx
@@ -544,40 +544,42 @@ export default function DashboardLayout({ children }) {
 
           {/* HiddenPost Search */}
           {location.pathname === '/profile/feedback-given' && questUtils.areHiddenPosts && (
-            <div className="relative my-[15px] ml-[31px] hidden h-fit w-[18.75rem] min-w-[18.75rem] rounded-[15px] bg-white py-[23px] pl-[1.3rem] pr-[2.1rem] dark:border dark:border-gray-100 dark:bg-gray-200 laptop:block">
-              <div className="relative h-[45px] w-full">
-                <input
-                  type="text"
-                  id="floating_outlined"
-                  className="peer block h-full w-full appearance-none rounded-[10px] border-2 border-[#707175] bg-transparent py-2 pl-5 pr-8 text-sm text-[#707175] focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-500 tablet:text-[18.23px]"
-                  value={hiddenSearch}
-                  placeholder=""
-                  onChange={handleHiddenPostSearch}
-                />
-                <label
-                  htmlFor="floating_outlined"
-                  className="absolute left-[15px] start-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm text-[#707175] duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-200 dark:text-white-100 peer-focus:dark:text-blue-500 tablet:text-[17px] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
-                >
-                  Search
-                </label>
+            <div className="my-[15px] ml-[31px] hidden h-fit w-[18.75rem] min-w-[18.75rem] rounded-[15px] bg-white py-[23px] pl-[1.3rem] pr-[2.1rem] dark:border dark:border-gray-100 dark:bg-gray-200 laptop:block">
+              <div className="relative">
+                <div className="relative h-[45px] w-full">
+                  <input
+                    type="text"
+                    id="floating_outlined"
+                    className="peer block h-full w-full appearance-none rounded-[10px] border-2 border-[#707175] bg-transparent py-2 pl-5 pr-8 text-sm text-[#707175] focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-500 tablet:text-[18.23px]"
+                    value={hiddenSearch}
+                    placeholder=""
+                    onChange={handleHiddenPostSearch}
+                  />
+                  <label
+                    htmlFor="floating_outlined"
+                    className="absolute left-[15px] start-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm text-[#707175] duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-200 dark:text-white-100 peer-focus:dark:text-blue-500 tablet:text-[17px] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
+                  >
+                    Search
+                  </label>
+                </div>
+                {getHiddenPostFilters.searchData && (
+                  <button
+                    className="absolute right-3 top-4"
+                    onClick={() => {
+                      dispatch(updateSearch(''));
+                    }}
+                  >
+                    <GrClose className="h-4 w-4 text-[#ACACAC] dark:text-white" />
+                  </button>
+                )}
+                {!getHiddenPostFilters.searchData && (
+                  <img
+                    src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/dashboard/search.svg`}
+                    alt="search"
+                    className="absolute right-3 top-4 h-4 w-4"
+                  />
+                )}
               </div>
-              {getHiddenPostFilters.searchData && (
-                <button
-                  className="absolute right-3 top-4"
-                  onClick={() => {
-                    dispatch(updateSearch(''));
-                  }}
-                >
-                  <GrClose className="h-4 w-4 text-[#ACACAC] dark:text-white" />
-                </button>
-              )}
-              {!getHiddenPostFilters.searchData && (
-                <img
-                  src={`${import.meta.env.VITE_S3_IMAGES_PATH}/assets/svgs/dashboard/search.svg`}
-                  alt="search"
-                  className="absolute right-3 top-4 h-4 w-4"
-                />
-              )}
             </div>
           )}
 
--- src/pages/features/news-feed/components/NewsFeedSearch.tsx
+++ src/pages/features/news-feed/components/NewsFeedSearch.tsx
@@ -29,21 +29,21 @@ export default function NewsFeedSearch() {
 
   return (
     <>
-      {location.pathname === '/news' && questUtils.areNewsFeed && (
+      {location.pathname === '/news' && (
         <div className="mx-4 my-2 h-fit w-[128px] rounded-[3.55px] bg-white dark:border dark:border-gray-100 dark:bg-gray-200 laptop:my-[15px] laptop:ml-[31px] laptop:block laptop:w-[18.75rem] laptop:min-w-[18.75rem] laptop:rounded-[15px] laptop:py-[23px] laptop:pl-[1.3rem] laptop:pr-[2.1rem]">
           <div className="relative">
             <div className="relative h-[18px] w-full laptop:h-[45px]">
               <input
                 type="text"
                 id="floating_outlined"
-                className="peer block h-full w-full appearance-none rounded-[3.55px] border border-[#707175] bg-transparent py-2 pl-5 pr-8 text-[6px] text-[#707175] focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-500 tablet:text-[18.23px] laptop:rounded-[10px] laptop:border-2 laptop:text-sm"
+                className="peer block h-full w-full appearance-none rounded-[3.55px] border border-[#707175] bg-transparent py-2 pl-5 pr-8 text-[6px] text-[#707175] focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-500 tablet:text-[18.23px] laptop:rounded-[10px] laptop:border-2 laptop:text-[18.23px]"
                 value={newsFeedSearch}
                 placeholder=""
                 onChange={handleNewsFeedSearch}
               />
               <label
                 htmlFor="floating_outlined"
-                className="absolute left-[15px] start-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-[8.33px] text-[#707175] duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-200 dark:text-white-100 peer-focus:dark:text-blue-500 tablet:text-[17px] laptop:text-sm rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
+                className="absolute left-[15px] start-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-[8.33px] text-[#707175] duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-200 dark:text-white-100 peer-focus:dark:text-blue-500 tablet:text-[17px] laptop:text-[17px] rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
               >
                 Search
               </label>
