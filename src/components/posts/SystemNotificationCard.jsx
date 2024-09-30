f14faf99c34cc1c70f038dd27e59e58dc61ad52b
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-09-30 15:09:03 +0500

    change dyk to open dialogue box

--- src/components/posts/SystemNotificationCard.jsx
+++ src/components/posts/SystemNotificationCard.jsx
@@ -1,15 +1,18 @@
 import { Button } from '../ui/Button';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { setGuestSignUpDialogue } from '../../features/extras/extrasSlice';
 
 const SystemNotificationCard = ({ post, innerRef }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   return (
     <div
-      className="dark:bg-blue-400 border-blue-500 bg-white-800 flex flex-col gap-2 rounded-[13.842px] border-2 px-7 pb-[15px] pt-[14px] dark:border-gray-300 tablet:gap-4 tablet:border-[3.5px] tablet:px-[44px] tablet:py-6"
+      className="flex flex-col gap-2 rounded-[13.842px] border-2 border-blue-500 bg-white-800 px-7 pb-[15px] pt-[14px] dark:border-gray-300 dark:bg-blue-400 tablet:gap-4 tablet:border-[3.5px] tablet:px-[44px] tablet:py-6"
       ref={innerRef}
     >
-      <h1 className="text-accent-700 text-[13px] font-bold leading-normal dark:text-gray-300 tablet:text-[22px]">
+      <h1 className="text-[13px] font-bold leading-normal text-accent-700 dark:text-gray-300 tablet:text-[22px]">
         {post.header}
       </h1>
       {post?.text?.map((item, index) => (
@@ -26,7 +29,11 @@ const SystemNotificationCard = ({ post, innerRef }) => {
             variant="hollow-submit"
             className="w-fit bg-white dark:bg-transparent"
             onClick={() => {
-              navigate(post.buttonUrl);
+              if (post.buttonUrl === '/guest-signup') {
+                dispatch(setGuestSignUpDialogue(true));
+              } else {
+                navigate(post.buttonUrl);
+              }
             }}
           >
             {post.buttonText}
