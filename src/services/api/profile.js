ac0d932a927ed0dd5a27172cf57bd0211fe9b457
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-10-26 17:29:49 +0500

    other profiles api integration

---
+++ src/services/api/profile.js
@@ -0,0 +1,24 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import api from '../api/Axios';
+
+const fetchProfiles = async (pageNo, limit = 5, sort = 'Newest First', terms = '') => {
+  const response = await api.get(`/user/searchUsersByDomain`, {
+    params: {
+      _page: pageNo,
+      _limit: limit,
+      sort,
+      terms,
+    },
+  });
+  return response.data;
+};
+
+export const useFetchOtherProfiles = (terms = '') => {
+  return useInfiniteQuery({
+    queryKey: ['news-feed', terms],
+    queryFn: async ({ pageParam = 1 }) => {
+      return await fetchProfiles(pageParam, 5, 'Newest First', terms);
+    },
+    getNextPageParam: (lastPage, allPages) => (lastPage.length ? allPages.length + 1 : undefined),
+  });
+};
