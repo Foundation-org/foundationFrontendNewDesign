2222c6ac62daa6f10ca0105045ab98b4116d55b9
Wamiq <wamiqakram@gmail.com>
2024-11-23 15:20:19 +0500

    widthrawls and deposits screen

--- src/pages/Dashboard/pages/Treasury/pages/deposits.tsx
+++ src/pages/Dashboard/pages/Treasury/pages/deposits.tsx
@@ -2,7 +2,7 @@ export default function Deposits() {
   return (
     <div className="h-[calc(100dvh-174px)] overflow-y-auto px-4 no-scrollbar tablet:h-[calc(100dvh-173.63px)] tablet:px-6 laptop:h-[calc(100dvh-96px)]">
       <p className="font-inter dark:text-gray mt-[3.319vh] text-center text-[5.083vw] font-bold text-[#9F9F9F] dark:text-gray-900 tablet:text-[4vw] laptop:text-[2.083vw]">
-        🚀 Deposits open on Nov 20, 2024! Get ready!
+        🚀 Deposits will be opening soon!
       </p>
     </div>
   );
