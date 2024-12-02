a25b622f0380a70fbf2ec008a82c0b0a23e7f1fc
mmahad911 <mmahad913@gmail.com>
2024-12-02 22:36:37 +0500

    uuid added.
    uuid added.

--- src/services/mutations/verification-adges.js
+++ src/services/mutations/verification-adges.js
@@ -83,6 +83,7 @@ const reOrderLinHubLinks = async (data) => {
   const response = await api.post('/updateBadgeDataArray', {
     type: 'linkHub',
     data,
+    uuid: localStorage.getItem('uuid')
   });
 
   return response.data;
